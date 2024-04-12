f7f2ec30e765c4eb14449075490a9ecbda040604
acktreptow <acktreptow@gmail.com>
2024-04-12 21:27:14 +0100

    update header css

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 function Header() {
   return (
-    <header>
-      <h1>This is the header</h1>
+    <header className="bg-blue-400 flex px-6 py-4 justify-between">
+      <h1>Sony First Party</h1>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          <Link href="/developers">Developers</Link>
+        </li>
+      </ul>
     </header>
   );
 }

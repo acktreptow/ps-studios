8ea769a1f47ca23df5f6edda65fb6ca9edc538ce
acktreptow <acktreptow@gmail.com>
2024-04-12 21:00:47 +0100

    create developers page

---
+++ src/app/developers/page.tsx
@@ -0,0 +1,33 @@
+function DevelopersPage() {
+  return (
+    <>
+      <h1>Sony First Party Developers</h1>
+      <ul>
+        <li>Bend Studio</li>
+        <li>Bluepoint Games</li>
+        <li>Bungie</li>
+        <li>Fabrik Games</li>
+        <li>Firesprite</li>
+        <li>Firewalk Studios</li>
+        <li>Guerilla Games</li>
+        <li>Haven Studios</li>
+        <li>Housemarque</li>
+        <li>Insomniac Games</li>
+        <li>Media Molecule</li>
+        <li>Naughty Dog</li>
+        <li>Neon Koi</li>
+        <li>Nixxes Software</li>
+        <li>Polyphony Digital</li>
+        <li>San Diego Studio</li>
+        <li>San Mateo Studio</li>
+        <li>Santa Monica Studio</li>
+        <li>Sucker Punch Productions</li>
+        <li>Team Asobi</li>
+        <li>Valkyrie Entertainment</li>
+        <li>XDev</li>
+      </ul>
+    </>
+  );
+}
+
+export default DevelopersPage;

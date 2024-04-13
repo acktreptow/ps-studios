43d15ba4eee5a1f1b97b0d77378c12b486cff45f
acktreptow <acktreptow@gmail.com>
2024-04-13 03:42:25 +0100

    fill out header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
+import psStudiosLogo from "../../../public/images/PlayStation_Studios_logo.svg";
 
 function Header() {
   return (
-    <header className="bg-blue-400 flex px-6 py-4 justify-between">
+    <header className="bg-blue-400 flex px-6 py-4 justify-between items-center">
       <h1>Sony First Party</h1>
-      <ul>
+      <Image src={psStudiosLogo} alt="PS Studios Logo" className="w-8" />
+      <ul className="flex gap-2">
         <li>
           <Link href="/">Home</Link>
+        </li>
+        <li>
           <Link href="/developers">Developers</Link>
         </li>
       </ul>

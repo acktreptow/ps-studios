1277e4822ef4b8aec53a969c3a80a61c7f948a60
acktreptow <acktreptow@gmail.com>
2024-04-19 06:28:30 +0100

    fill out bend studio page

--- src/app/developers/bendstudio/page.tsx
+++ src/app/developers/bendstudio/page.tsx
@@ -2,7 +2,37 @@ function BendStudioPage() {
   return (
     <>
       <h1>Bend Studio</h1>
-      <p>Developers of Days Gone</p>
+      <p>
+        Founded in 1992, this Oregon-based developer is best known for
+        developing the Syphon Filter series and Days Gone.
+      </p>
+      <p>
+        Bend Studio have not released a native PS5 game yet. However, Days Gone
+        did receive a 60fps patch that doubles the framerate of the PS4
+        application.
+      </p>
+      <p>
+        Despite Days Gone releasing in April 2019, we still know very little
+        about their next title. Here's what we do know:
+      </p>
+      <ul>
+        <li>It will be a new IP</li>
+        <li>It will be a live service game</li>
+        <li>
+          Bend did initially pitch Days Gone 2, which was rejected by Sony due
+          to poor critical reception.
+        </li>
+        <li>
+          The main people in charge will not be the same as the people who led
+          Days Gone. This is because they lost the internal power struggle which
+          occured after Days Gone launched.
+        </li>
+      </ul>
+      <p>
+        We will hopefully here more soon as it has already been 5 years since
+        the last game. At the very least, it should be a very different game on
+        the writing side, as Bend's main writers are no longer with the studio.
+      </p>
     </>
   );
 }

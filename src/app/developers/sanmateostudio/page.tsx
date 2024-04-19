b9233cb24f0eeaf75a91e12c410f91a21f5c0b99
acktreptow <acktreptow@gmail.com>
2024-04-19 20:44:48 +0100

    filled out San Mateo page

--- src/app/developers/sanmateostudio/page.tsx
+++ src/app/developers/sanmateostudio/page.tsx
@@ -3,8 +3,18 @@ function SanMateoStudioPage() {
     <>
       <h1>San Mateo Studio</h1>
       <p>
-        San Mateo Studio are a support studio and thus have no game series for
-        which they are primarily known for.
+        Founded in 1998, San Mateo Studio (formerly Foster City Studio) works
+        out of... San Mateo, California.
+      </p>
+      <p>
+        Like a few other PlayStation first-party developers, San Mateo are only
+        a support studio. They did spin off a small team in 2014 that was called
+        Pixelopus, but this first-party studio was closed down in June 2023.
+      </p>
+      <p>
+        Due to their support status, they've not released a PS5 game and it's
+        incredibly unlikely they will ship one where they are the main
+        developer.
       </p>
     </>
   );

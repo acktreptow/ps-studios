d4a61b7ded8bf5a730ec4dd74005ddac41c459bc
acktreptow <acktreptow@gmail.com>
2024-04-20 00:00:48 +0100

    fill out guerilla page

--- src/app/developers/guerillagames/page.tsx
+++ src/app/developers/guerillagames/page.tsx
@@ -4,7 +4,33 @@ function GuerillaGamesPage() {
   return (
     <>
       <h1>Guerilla Games</h1>
-      <p>Developers of the Killzone and Horizon series</p>
+      <h2>History</h2>
+      <p>
+        Founded in 2000, this Amsterdam-based developer is primarily known for
+        its FPS series Killzone and third-person adventure series Horizon.
+      </p>
+      <p>They were purchased by Sony in 2005.</p>
+      <p>
+        So far, the only game Guerilla have released for the PS5 was the
+        cross-gen title 'Horizon Forbidden West' that released in early 2022.
+        Its DLC, which released in 2023, was however a PS5 exclusive.
+      </p>
+      <p>
+        Since the Burning Shores released, we haven't had any updates on their
+        next game.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        Despite no information to go off of, due to the popularity of the
+        Horizon series and how it elevated the developer to a new tier above
+        their Killzone routes, their next game is almost certainly a third
+        Horizon game.
+      </p>
+      <p>
+        Fortunately this time, you don't have to put too much stock into my
+        analysis. This game will almost certainly be very similar to 2022's
+        Forbidden West. So try the game out and see if you like it!
+      </p>
     </>
   );
 }

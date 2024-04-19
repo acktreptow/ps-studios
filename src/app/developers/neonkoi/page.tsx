8749db1ca38b5578bc4481943c22215709faafb7
acktreptow <acktreptow@gmail.com>
2024-04-19 19:40:55 +0100

    fill out neon koi page

--- src/app/developers/neonkoi/page.tsx
+++ src/app/developers/neonkoi/page.tsx
@@ -3,9 +3,30 @@ function NeonKoiPage() {
     <>
       <h1>Neon Koi</h1>
       <p>
-        Neon Koi, previously Savage Game Studios, is a new studio formed by
-        former Sony Interactive Entertainment employees. They are currently
-        working on an unannounced project.
+        Neon Koi is a German-Finnish developer that was founded in 2020 under
+        their initial name - Savage Game Studios. It was in fact under this old
+        name when they originally purhcased by Sony in August, 2022.
+      </p>
+      <p>
+        In regards to their PS5 output, they haven't released anything, nor
+        should you expect them to. This is because Neon Koi are a founding
+        member of PlayStation Studios' Mobile Division and were purchase with
+        the sole intent to create mobile games.
+      </p>
+      <p>
+        However, if you're still interested in their mobile offerings, I still
+        wouldn't hold my breath. They've yet to even announce a game let alone
+        show gameplay or release one. In fact, Neon Koi's only times in the news
+        were for negative headlines. For example, when the head of the Mobile
+        Divison left, which was quickly followed up by a founding member leaving
+        under suspicious ciorcumstances as well.
+      </p>
+      <p>
+        The last time they were in the news was when the developer officially
+        changed the name to Neon Koi in November 2023. With this change and a
+        revamped website, hopefully it means we get a game announcement soon. We
+        can then start determining if Neon Koi and PlayStation's Mobile Divison
+        as a whole are worth keeping an eye on.
       </p>
     </>
   );

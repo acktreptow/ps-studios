2589bdf08363710466b6d76b56c8c3308f834d3a
acktreptow <acktreptow@gmail.com>
2024-04-19 23:20:26 +0100

    fill out bluepoint page

--- src/app/developers/bluepointgames/page.tsx
+++ src/app/developers/bluepointgames/page.tsx
@@ -4,7 +4,34 @@ function BluepointGamesPage() {
   return (
     <>
       <h1>Bluepoint Games</h1>
-      <p>Bluepoint Games is a video game developer based in Austin, Texas.</p>
+      <h2>History</h2>
+      <p>
+        Founded in 2006, this Texas-based developer is primarily known for its
+        excellent remasters and remakes. The majority of which are from Sony's
+        firrst-party IP. These include remastering God of War 1 and 2 to the PS3
+        and Uncharted 1-3 to the PS4.
+      </p>
+      <p>
+        The only non-Sony IP remaster was MGS 2 and 3 to both the PS3 and Xbox
+        360. However, they have provided support on other non-PlayStation
+        products such as the Xbox 360 version of Titanfall.
+      </p>
+      <p>
+        Bluepoint's most recent projects have however been full-on remakes that
+        include Shadow of the Collossus on PS4 and most recently, Demon's Souls
+        on the PS5. This was a launch title for the PS5 and was highly praised.
+        We have haven't even gotten the name of their next game despite it being
+        over 3 years later.
+      </p>
+      <h2>My Analysis</h2>
+      <p>
+        The only thing we know about their next game is that it's 'original
+        content'. However, despite know very little, there is every reason to be
+        confident. Bluepoint Games have an excellent track record and do have
+        some experience working on original content as a suppoer studio for Sant
+        Monica's God of War: Ragnarok.
+      </p>
+      <h2>Games</h2>
     </>
   );
 }

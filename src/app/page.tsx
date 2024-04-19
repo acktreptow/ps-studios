5a02201363e451e966ebd92fbfa1142228e45652
acktreptow <acktreptow@gmail.com>
2024-04-19 04:19:10 +0100

    update developers json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import playstationLogo from "../../public/images/playstation-logo.png";
-import xdevLogo from "../../public/images/xdev-logo.png";
-import ps5Logo from "../../public/images/ps5-logo.png";
 import developers from "../app/data/developers.json";
 
 function Homepage() {
@@ -41,40 +38,17 @@ function Homepage() {
         </button>
       </form>
       <div className="grid grid-cols-3 mx-2 mt-10 mb-4">
-        {developers
-          .filter((developer) => developer.name !== "XDev")
-          .map((developer) => (
-            <Link key={developer.id} href={`/developers/${developer.urlPath}`}>
-              <Image
-                src={developer.img}
-                width={500}
-                height={500}
-                alt={`${developer.name} Logo`}
-                className="transform scale-90 rounded-lg"
-              />
-            </Link>
-          ))}
-        <Link href="#">
-          <Image
-            src={playstationLogo}
-            alt="PlayStation Logo"
-            className="transform scale-90 rounded-lg"
-          />
-        </Link>
-        <Link href="/developers/xdev">
-          <Image
-            src={xdevLogo}
-            alt="XDEV Logo"
-            className="transform scale-90 rounded-lg"
-          />
-        </Link>
-        <Link href="/about">
-          <Image
-            src={ps5Logo}
-            alt="PS5 Logo"
-            className="transform scale-90 rounded-lg"
-          />
-        </Link>
+        {developers.map((developer) => (
+          <Link key={developer.id} href={`/developers/${developer.urlPath}`}>
+            <Image
+              src={developer.img}
+              width={500}
+              height={500}
+              alt={`${developer.name} Logo`}
+              className="transform scale-90 rounded-lg"
+            />
+          </Link>
+        ))}
       </div>
     </main>
   );

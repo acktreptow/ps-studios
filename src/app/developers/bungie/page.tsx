7050b5e738dbbd8b7190719359fcbc08c96cc295
acktreptow <acktreptow@gmail.com>
2024-04-19 23:37:21 +0100

    fill out Bungie page

--- src/app/developers/bungie/page.tsx
+++ src/app/developers/bungie/page.tsx
@@ -4,7 +4,53 @@ function BungiePage() {
   return (
     <>
       <h1>Bungie</h1>
-      <p>Developers of Destiny and Halo</p>
+      <h2>History</h2>
+      <p>
+        Founded in 1991, this Washington-based developer is perhaps the most
+        unique first-party developer for a variety of reasons:
+      </p>
+      <ul>
+        <li>
+          They are the creators of the Xbox's flagship IP in Halo and were once
+          owned by Microsoft.
+        </li>
+        <li>
+          After leaving Microsoft in 2007, they then joined Activision in 2010
+          to work on Destiny.
+        </li>
+        <li>
+          They are the only developer technically not part of PS Studios.
+          Instead, acting as its own independent subsidiary under Sony.
+        </li>
+        <li>
+          They are the only first-party developer who is allowed to launch all
+          of their games simultaneously across competing platforms like Xbox.
+        </li>
+      </ul>
+      <p>
+        The reasons for the past 2 points is because Sony was desperate to bring
+        in-house knowledge of live service games and even at the bloated price
+        of $3.6 billion, these were stipulations in order to sell, which they
+        did so in 2022.
+      </p>
+      <h2>My Analysis</h2>
+      <p>
+        So far, the only game Bungie has brought to the PS5 is a port of Destiny
+        2 which launched in 2020. They have however announced their next game, a
+        live-service extraction shooter called Marathon, that was announced in
+        2023. We haven't seen anything since that original CG trailer.
+      </p>
+      <p>
+        There is every reason to be wary of it's quality though. Despite Sony
+        paying a king's ransom for their live service knowledge, Destiny 2 has
+        been severely underperforming. To the point where there were huge
+        layoffs just to ensure certain targets were met that prevented a Sony
+        takeover of Bungie's board. It's almost a certainty that another round
+        of layoffs will occur after Destiny 2's last expansion is released. And
+        even then, Sony might still be able to take over as they are bleeding so
+        much money. It's hard to believe Marathon will be the game they need it
+        to be under those circumstances.
+      </p>
     </>
   );
 }

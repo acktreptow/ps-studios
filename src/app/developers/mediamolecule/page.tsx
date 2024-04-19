221326f7f56b83d58ba9c4014d131b6ec57bd2f3
acktreptow <acktreptow@gmail.com>
2024-04-19 18:56:57 +0100

    fill out media molecule page

--- src/app/developers/mediamolecule/page.tsx
+++ src/app/developers/mediamolecule/page.tsx
@@ -2,7 +2,30 @@ function MediaMoleculePage() {
   return (
     <>
       <h1>Media Molecule</h1>
-      <p>Developers of the LittleBigPlanet, Tearaway and Dreams series</p>
+      <p>
+        Founded in 2006, this Surrey-based developer is primarily known for
+        their critically acclaimed LittleBigPlanet series that launched on the
+        PS3, as well as 2020&#39;s Dream for the PS4.
+      </p>
+      <p>
+        Media Molecule has yet to release a PS5 title. In fact, we don't even
+        have a name for their game, let alone gameplay, despite their last game
+        releasing over 4 years ago.
+      </p>
+      <p>
+        Whatever their next game is, I wouldn't expect much as the developer is
+        far removed from it's heyday during the PS3 generation. Dreams wasn't
+        close to a commercial success, they have lost the vast majoirty of their
+        founders, and it's been reported that they were nearly close instead of
+        London Studio during Sony's October 2023 layoffs.
+      </p>
+      <p>
+        With rising development costs, general inflation, and the type of games
+        Media Molecule make not being commercially viable, It's far more likely
+        the studio will close before it releases a new game. Here's hoping I'm
+        wrong, as Sony needs a diverse portfolio of first-party games that Media
+        Molecule will bring.
+      </p>
     </>
   );
 }

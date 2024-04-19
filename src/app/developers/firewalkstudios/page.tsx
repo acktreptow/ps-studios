e86110c96172d2ac96ae6da71adc45458f4c2317
acktreptow <acktreptow@gmail.com>
2024-04-19 23:51:09 +0100

    fill out firewalk page

--- src/app/developers/firewalkstudios/page.tsx
+++ src/app/developers/firewalkstudios/page.tsx
@@ -4,7 +4,27 @@ function FirewalkStudiosPage() {
   return (
     <>
       <h1>Firewalk Studios</h1>
-      <p>Developers of the upcoming Concord</p>
+      <h2>History</h2>
+      <p>
+        Founded in 2018, this Washington-based developer is led by developers
+        with significant experience in popular multiplayer shooter IP such as
+        Halo and Destiny.
+      </p>
+      <p>
+        With this speciality and Sony's increased focus to expand their
+        multiplayer portfilio this generation, it's no surprise that they
+        officially joined PlayStation Studios in 2023 and unveiled its debut
+        project: Concord. A live-service PVP shooter with an 80's sci-fi
+        aesthetic that will launch on PS5 and PC in 2024.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        Everything we know about Concord is written above, so suffice to say we
+        don't have a lot to go off. Nonetheless, the pedigree of the developers
+        before the creation of Firewalk is enough for you to be excited about
+        Concord. As the game is still scheduled for a 2024 release, we should
+        hopefully see something more concrete soon.
+      </p>
     </>
   );
 }

f6b3dfea0a36fd9298ffaeb214f4233227c566a3
acktreptow <acktreptow@gmail.com>
2024-04-19 19:49:00 +0100

    fill out valkyrie page

--- src/app/developers/valkyrieentertainment/page.tsx
+++ src/app/developers/valkyrieentertainment/page.tsx
@@ -2,7 +2,22 @@ function ValkyrieEntertainmentPage() {
   return (
     <div>
       <h1>Valkyrie Entertainment</h1>
-      <p>Valkyrie Entertainment is a first party support studio</p>
+      <p>
+        Founded in 2002 and officially purchased by Sony in 2021, this
+        Seattle-based developer acts as a support studio for other first-party
+        titles being developed by other PlayStation Studios.
+      </p>
+      <p>
+        In fact despite being around for over 20 years, the only game they were
+        the main developer on was 2015's digital only game "Guns Up!", which
+        came and went with no fanfare. You're far more likely to see their
+        support work on huge titles like God of War: Ragnarok.
+      </p>
+      <p>
+        Valkyrie have therefore not released any games for the PS5 and it's
+        highly likely they won't ever. Especially if their unsecure barebones
+        website is anything to go by.
+      </p>
     </div>
   );
 }

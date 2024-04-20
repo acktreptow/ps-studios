e135233378e9710570a67d34a7d4c86ec6ce4d21
acktreptow <acktreptow@gmail.com>
2024-04-20 16:45:29 +0100

    fill out polyphony page

--- src/app/developers/polyphonydigital/page.tsx
+++ src/app/developers/polyphonydigital/page.tsx
@@ -4,9 +4,31 @@ function PolyphonyDigitalPage() {
   return (
     <>
       <h1>Polyphony Digital</h1>
+      <h2>History</h2>
       <p>
-        Developers of the hit racing series Gran Turismo as well as Tourist
-        Trophy and PS1 classic Omega Boost
+        Founded in 1998, this Tokyo-based developer are known for the hit racing
+        series Gran Turismo.
+      </p>
+      <p>
+        They've released one game for the PS5 so far. The cross-gen title "Gran
+        Turismo 7" that came out in March 2022. Despite some issues at launch,
+        some brought on by themselves, the game was critically acclaimed and
+        sold very well. It's also widely seen as the premier experience on
+        PSVR2.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        Considering it takes around 4 years for Polyphony to release a new Gran
+        Turismo, I imagine we'll almost see a Gran Turismo 8 towards the end of
+        the PS5 generation. This one will be PS5-only and should also take a
+        huge graphics leap with the PS5 Pro already out by then.
+      </p>
+      <p>
+        If you're not a driving game fan, I would move on as I highly doubt
+        we'll get something else from Polyphony any time soon. Their last non
+        Gran Turismo was 2006's Tourist Trophy which was essentially Gran
+        Turismo but motorbikes. We're a LONG way from a non-racing game from
+        Polyphony with 1999's Omega Boost.
       </p>
     </>
   );

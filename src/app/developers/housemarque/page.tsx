84e3d535e7db6f57bcfa8f531e19fd0f35d6685d
acktreptow <acktreptow@gmail.com>
2024-04-20 05:43:33 +0100

    fill out housemarque page

--- src/app/developers/housemarque/page.tsx
+++ src/app/developers/housemarque/page.tsx
@@ -4,7 +4,37 @@ function HousemarquePage() {
   return (
     <>
       <h1>Housemarque</h1>
-      <p>Developers of the 2021 title Returnal</p>
+      <h2>History</h2>
+      <p>
+        Founded in 1995, this Finnish-based developer has released over 10
+        different IP on PlayStation consoles since 2007. This includes Super
+        Stardust, Resogun, and Matterfall.
+      </p>
+      <p>
+        Their most recent game is 2021's Returnal for the PS5, which took many
+        of the rouge-like elements seen in their previous titles, but were able
+        to now reach more mainstream demographic with a change to a third-person
+        shooter and more emphasis on a high-production value story.
+      </p>
+      <p>
+        Clearly Sony were happy with the outcome as they purchased Housemarque
+        within 2 month's of Returnal's release.
+      </p>
+      <h2>My Analysis</h2>
+      <p>
+        Returnal is the only game Housemarque have released on the PS5, and
+        despite it releasing over 3 years ago, we don't even have a title for
+        their next game.
+      </p>
+      <p>
+        Nonetheless, there's every reason to be excited about whatever it is.
+        Housemarque proved they could put their difficult rouge-like bona fides
+        into a more mainstream package with Returnal. Now with this new
+        experience under their belt, plus the backing of Sony as an official
+        first-party developer, their next game should improve on this new
+        formula. It has been 3 years though, so we should (hopefully) hear about
+        it soon.
+      </p>
     </>
   );
 }

dfd72067f8e7fcc9919fa47d2fb50be3a76146d8
acktreptow <acktreptow@gmail.com>
2024-04-20 18:29:12 +0100

    fill out sucker punch page

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -4,7 +4,36 @@ function SuckerPunchProductionsPage() {
   return (
     <>
       <h1>Sucker Punch Productions</h1>
-      <p>Developers of the Sly Cooper, inFamous and Ghost of Tsushima series</p>
+      <h2>History</h2>
+      <p>
+        Sucker Punch Productions are a Washington-based developer that was
+        founded in 1997. This was to initially work on an N64 called Rocket:
+        Robot on Wheels.
+      </p>
+      <p>
+        They then started having their games funded by Sony, where they made the
+        Sly trilogy for the PS2 before switching to inFamous with the PS3
+        generation. It was during this time that they were purchased by Sony in
+        2011.
+      </p>
+      <p>
+        Sucker Punch have since moved to Ghost of Tsushima, which initially
+        released for the PS4. However, a Director's Cut that featured a mini
+        campaign was released on the PS5 in August 2021. This is currently their
+        only PS5 game.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        It's been nearly 4 year since Ghost of Tsushima was released to
+        widespread critical and commercial acclaim that catapulted the studio to
+        a higher tier in the industry. All rumours suggest that their next game
+        will also be direct sequel.
+      </p>
+      <p>
+        Considering these factors, ideally, the game should be announced very
+        soon. And rest assured, it will be yet another excellent game based off
+        Sucker Punch's track record.
+      </p>
     </>
   );
 }

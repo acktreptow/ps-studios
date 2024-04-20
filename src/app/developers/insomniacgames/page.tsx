97fe6eeeae0b7756b70b75a022bb5f35917a17fd
acktreptow <acktreptow@gmail.com>
2024-04-20 05:55:19 +0100

    fill out Insomniac page

--- src/app/developers/insomniacgames/page.tsx
+++ src/app/developers/insomniacgames/page.tsx
@@ -4,7 +4,43 @@ function InsomniacGamesPage() {
   return (
     <>
       <h1>Insomniac Games</h1>
-      <p>Developers of the Spider-Man and Ratchet & Clank series</p>
+      <h2>History</h2>
+      <p>
+        Founded in 1994, this California-based developer was basically a
+        PlayStation exclusive developer for decades before being officially
+        purchased by Sony in 2019.
+      </p>
+      <p>
+        This is because the 3 main franchises they were originally known for in
+        Spyro, Ratchet & Clank and Resistance were all exclusive to Sony
+        platforms and funded by them. It was only when the latter two IP started
+        to flounder in the PS3 generation when the developer branched out with
+        games by EA, Xbox Game Studios and Oculus.
+      </p>
+      <p>
+        The developer would find their footing again with 2018's Spider-Man that
+        ended up being the highest-selling Sony game during the PS4 generation.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        This is an easy one - they've been by far, the most important
+        first-party developer during the PS5 generation and the vast majority of
+        people would agree. This is because they've already released 4 games, 2
+        of which were new Spider-Man games, that all released to critical and
+        commercial success.
+      </p>
+      <p>
+        And even though Insomniac have released 4x the amount of titles compared
+        to many other Sony developers, we already know their next game will be.
+        Marvel's Wolverine that will most likely release in 2025. Between the
+        pedigree they've proven with Marvel properties through Spider-Man and
+        the huge leaks that unfortunately were released, it's safe to say that
+        this game will be another huge critical and commercial hit for them.
+      </p>
+      <p>
+        I really can't see them not being the most important first-party
+        developer for the PS5 generation when all is said and done.
+      </p>
     </>
   );
 }

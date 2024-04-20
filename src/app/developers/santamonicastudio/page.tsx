9fd4a88fe430a19394d6af5fe5560ad1ae83f36f
acktreptow <acktreptow@gmail.com>
2024-04-20 18:01:49 +0100

    fill out santa monica page

--- src/app/developers/santamonicastudio/page.tsx
+++ src/app/developers/santamonicastudio/page.tsx
@@ -4,7 +4,33 @@ function SantaMonicaStudioPage() {
   return (
     <>
       <h1>Santa Monica Studio</h1>
-      <p>Developers of the long-running award-winning God of War series.</p>
+      <h2>History</h2>
+      <p>
+        Santa Monica Studio is an LA-based developer that was founded in 1999.
+        Outside of its first game, 2001's Kinetica, the studio has exclusively
+        developed games for its hugely successful God of War series.
+      </p>
+      <p>
+        Their most recent game was 2022's God of War Ragnarok - a cross-gen
+        title and the only game they have released on the PS5 so far. Like the
+        2018 requel (thanks Scream 5 for this term), it was a big critical and
+        commercial success.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        As of right now, we don't know what Santa Monica's next game will be
+        aside from the fact that it will third-person. There's rumours that
+        predict a new IP, a sequel to Ragnarok, and a smaller-scope Gof of War
+        game in the vein of Spider-Man Miles-Morales.
+      </p>
+      <p>
+        Regardless of what it is, there's no reason to think it won't be a great
+        game based off their last 2 projects. The new God of Wars were so
+        critically acclaimed that many believe they helped usurp Naughty Dog as
+        Sony's premier developer. Hopefully we hear something more concrete too,
+        although Ragnarok only came out 1.5 years ago. There's A LOT of Sony
+        developers that need to talk first as their going on 4-5 without a game.
+      </p>
     </>
   );
 }

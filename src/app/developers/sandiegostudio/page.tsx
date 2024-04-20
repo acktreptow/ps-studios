2d10df6a3e1e094fc93ef2b43cf79df5c1bcb363
acktreptow <acktreptow@gmail.com>
2024-04-20 16:56:27 +0100

    fill out san diego page

--- src/app/developers/sandiegostudio/page.tsx
+++ src/app/developers/sandiegostudio/page.tsx
@@ -4,7 +4,38 @@ function SanDiegoStudioPage() {
   return (
     <>
       <h1>San Diego Studio</h1>
-      <p>Developers of the long-running MLB The Show baseball series</p>
+      <h2>History</h2>
+      <p>
+        Founded in 2001, San Diego Studio is (obviously) a San Diego-based
+        first-party developer that is responsible for the MLB: The Show series.
+      </p>
+      <p>
+        This annual baseball series is the only series they work on. So
+        naturally, there's been 4 baseball games for the PS5 that have all come
+        out around late-March/early-April over the past 4 years.
+      </p>
+      <p>
+        This generation has been different though. As despite being a Sony
+        first-party developer, the MLB have forced PlayStation to develop an
+        Xbox and Nintendo port since MLB 21 or they would lost the license.
+        Clearly, turning the series platform was the better alternative to
+        losing the license as the series has continued.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        This ones easy to predict. San Diego Studio haven't developed a non-MLB
+        game for close to a decade. And with rising development costs couple
+        with tighter profits for PlayStation, it's unlikely they would be
+        allowed to experiment with something new, and that's assuming they even
+        want to.
+      </p>
+      <p>
+        We're also 4 games deep into this series being multiplatform and nothing
+        materially has changed. With there being an Xbox and Switch version,
+        that was the one change that could have changed the series but didn't.
+        It's therefore safe to assume San Diego will continue with a yearly
+        multiplatform MLB game for the foreseeable future.
+      </p>
     </>
   );
 }

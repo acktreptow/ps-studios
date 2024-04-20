440c95147e90d2a14b40a8de66a81e398615fd52
acktreptow <acktreptow@gmail.com>
2024-04-20 20:10:03 +0100

    fill out asobi page

--- src/app/developers/teamasobi/page.tsx
+++ src/app/developers/teamasobi/page.tsx
@@ -4,7 +4,42 @@ function TeamAsobiPage() {
   return (
     <>
       <h1>Team Asobi</h1>
-      <p>Developers of the PS5 launch title Astros Playroom</p>
+      <h2>History</h2>
+      <p>
+        Team Asobi is a Tokyo-based developer that technically, was formed in
+        2021. They were originally formed in 2012 as part of Japan Studio, but
+        became independent once Japan Studio was shut down in 2020.
+      </p>
+      <p>
+        The developer are primarily known for the smaller-scoped games that
+        focus on new PlayStation hardware. This was seen with The PlayRoom to
+        show off the DualShock 4 and Astro Bot Rescue Mission to show off PSVR.
+      </p>
+      <p>
+        Their most recent game is Astro's Playroom. A free PS5 launch title that
+        comes pre-installed. Its platforming was developer in a way to show off
+        the consoles new features like haptic feedback.
+      </p>
+      <p>My analysis</p>
+      <p>
+        Despite their last game releasing over 3 years ago, with PSVR2 releasing
+        during that time frame, we still haven't officially heard anything about
+        their next game.
+      </p>
+      <p>
+        However, there are rumors that there next game will have a short
+        marketing cycle and will release towards the end of 2024. It will also
+        be slightly larger in scale, and playable on both the TV screen and
+        through PSVR2, taking advantage of both hardware.
+      </p>
+      <p>
+        Astro's Playroom was an incredible game that really showed off next-gen
+        hardware in a tight package. After all, it was the reason they survived
+        the closure of Japan Studio. With their next game also having double the
+        development time as their used to, it should be a great game that
+        hopefully showcases PSVR2 as the headset needs a win. Hopefully it is
+        true that it comes out this year too.
+      </p>
     </>
   );
 }

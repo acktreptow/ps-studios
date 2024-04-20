e123a6312b74df3b5a4262d50935209bef5278cd
acktreptow <acktreptow@gmail.com>
2024-04-20 15:54:34 +0100

    fill out nixxes page

--- src/app/developers/nixxessoftware/page.tsx
+++ src/app/developers/nixxessoftware/page.tsx
@@ -4,8 +4,38 @@ function NixxesSoftwarePage() {
   return (
     <>
       <h1>Nixxes Software</h1>
+      <h2>History</h2>
       <p>
-        Nixxes software are primarily a support studio for PC versions of games.
+        Founded in 1999, this this Utrecht-based studio holds a very unique
+        position among PlayStation Studios ever since they were purchased in
+        2021.
+      </p>
+      <p>
+        Unlike all other PlayStation developers who are there to work on new
+        games, Nixxes's sole purpose is to work on PC ports on PS5 games.
+      </p>
+      <p>
+        Since 2022, they've already released 4 ports of PS5 games with all the
+        bells and whistles you'd expect to see in a PC version, with a 5th game
+        coming next month. These ports have also be far more critically
+        acclaimed compared to other PC ports of PS5 games that were done
+        in-house such as The Last of Us Part II PC by Naughty Dog.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        Nixxes are one of the most important companies for PlayStation as they
+        are the way Sony finds growth for these big AAA games outside of the
+        console market. Something needed as development costs continue to rise.
+        I highly doubt we will see day-1 PC ports for single-player games
+        however from them. Logistically, it just doesn't work as an external
+        studio.
+      </p>
+      <p>
+        For PS5 fans? There's also a reason to keep an eye on them! In 2023,
+        Nixxes confirmed that it would also start working on remasters as well
+        as PC ports. So we should hopefully see some great PS3/PS4 games that
+        could do with a fresh coat of paint, receive a definitive version by the
+        expertise of this developer.
       </p>
     </>
   );

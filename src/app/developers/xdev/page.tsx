7a67d07f55ce7dd5914067a348cc479ac2ed53e5
acktreptow <acktreptow@gmail.com>
2024-04-20 23:22:26 +0100

    fill out xdev page

--- src/app/developers/xdev/page.tsx
+++ src/app/developers/xdev/page.tsx
@@ -4,7 +4,32 @@ function Xdevpage() {
   return (
     <>
       <h1>XDev</h1>
-      <p>XDev work on second-party deals for PlayStation</p>
+      <h2>History</h2>
+      <p>
+        Formed in 2000, XDev have a very unique role amongst PlayStation
+        developers. This is because they don't develop games themselves, but
+        rather they collaborate with external developers to bring their games to
+        PlayStation platforms.
+      </p>
+      <p>
+        This type of development used to be called second-party. But both Xbox
+        and PlayStation now see them as first-party as they're still funcing the
+        games, so we will too now.
+      </p>
+      <p>
+        Due to working with so many different developers, XDev can produce an
+        eccelectic slection of games. On the PS5 alone, they've created a car
+        combat game called Destruction All Stars, a platformer with Sackboy's
+        Big Advanture, a multiplayer live-service game with Helldivers 2 a VR
+        FPS with Firewall Ultra and a soulsborne game with Rise of the Ronin.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        XDev currently have 3 games publicly in development. Stellar Blade,
+        Until Dawn Remake and Death Stranding 2. Even though their games have
+        been hit or miss this generation, as these are sequels and remakes, it
+        should be far more likely that they hit.
+      </p>
     </>
   );
 }

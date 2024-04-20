894e9e3a5c97bcf2e80cba55fb21f8d5ebc51fb4
acktreptow <acktreptow@gmail.com>
2024-04-20 06:09:42 +0100

    fill out naughty dog page

--- src/app/developers/naughtydog/page.tsx
+++ src/app/developers/naughtydog/page.tsx
@@ -4,7 +4,46 @@ function NaughtyDogPage() {
   return (
     <>
       <h1>Naughty Dog</h1>
-      <p>Developers of the Uncharted and The Last of Us series</p>
+      <h2>History</h2>
+      <p>
+        Naughty Dog are a Santa Monica, California-based developer that was
+        founded all the way back in 1984 - before Nintendo had ever released a
+        game!
+      </p>
+      <p>
+        They are also one of the earliest PlayStation Studios as they were
+        founded in 2001. Due to the incredible success of their Crash Bandicoot
+        trilogy that was essentially the Super Mario IP of the PS1.
+      </p>
+      <p>
+        After a decent PS2 generation with their Jak & Daxter series, Naughty
+        Dog's prestige would shoot into the stratosphere during the PS3's
+        generation. First with their Uncharted trilogy before basically becoming
+        the premier PlayStation Studio with The Last of Us. This continued into
+        the PS4 generation with sequels in both IP.
+      </p>
+      <h2>My analysis</h2>
+      <p>
+        Technically, Naughty Dog are tied with Insomniac with putting the most
+        PS5 titles out at 4. However unlike, Insomniac, all four games were
+        either remasters or remakes of the most recent Uncharted games and both
+        Last of Us titles.
+      </p>
+      <p>
+        Furthermore, the only future new title we knew anything about was The
+        Last of Us Online, which was officially cancelled around 3.5 years into
+        development. It's therefore hard to be particularly confident in the
+        developer despite the pedigree they've shown over the past 15 years.
+        While the remakes/remasters were nice, one could easily argue they were
+        unnecessary due to the original titles still accessible on modern
+        console and still play great.
+      </p>
+      <p>
+        I could easily see us getting one new game max from them this generation
+        which would be a real shame and quite the drop-off. You could already
+        name numerous other developers that are more important currently, which
+        would have been unthinkable when The Last of Us released in 2013.
+      </p>
     </>
   );
 }

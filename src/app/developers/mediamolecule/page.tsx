c8f631b456a56e3595959e180f39cfc71cc3cb37
acktreptow <acktreptow@gmail.com>
2024-04-22 06:33:56 +0100

    add image css and near final description to media molecule page

--- src/app/developers/mediamolecule/page.tsx
+++ src/app/developers/mediamolecule/page.tsx
@@ -1,34 +1,64 @@
-import games from "../../data/games.json";
+import Image from "next/image";
+import sackboy from "../../../../public/images/sackboy.png";
 
 function MediaMoleculePage() {
   return (
-    <>
-      <h1>Media Molecule</h1>
-      <p>
-        Founded in 2006, this Surrey-based developer is primarily known for
-        their critically acclaimed LittleBigPlanet series that launched on the
-        PS3, as well as 2020&#39;s Dream for the PS4.
+    <div className="bg-white text-gray-700 p-10">
+      <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
+        Media Molecule
+      </h1>
+      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+        <span className="border-b-2 border-gray-300">History</span>
+      </h2>
+      <p className="mb-5 text-lg">
+        Founded in 2006, this Surrey-based developer is primarily known for the
+        critically acclaimed LittleBigPlanet series that started on the PS3 in
+        2008.
       </p>
-      <p>
-        Media Molecule has yet to release a PS5 title. In fact, we don't even
-        have a name for their game, let alone gameplay, despite their last game
-        releasing over 4 years ago.
+      <p className="mb-8 text-lg">
+        Media Molecule&#39;s last title was 2020&#39;s Dreams for the PS4, and
+        therefore has yet to release a PS5 title. In regards to their next game,
+        we don&#39;t even have a name, let alone gameplay, despite Dreams
+        releasing 4 years ago.
       </p>
-      <p>
-        Whatever their next game is, I wouldn't expect much as the developer is
-        far removed from it's heyday during the PS3 generation. Dreams wasn't
-        close to a commercial success, they have lost the vast majoirty of their
-        founders, and it's been reported that they were nearly close instead of
-        London Studio during Sony's October 2023 layoffs.
+      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+        <span className="border-b-2 border-gray-300">
+          My Analysis and Predictions
+        </span>
+      </h2>
+      <p className="mb-5 text-lg">
+        Whatever Media Molecule&#39;s next game is, I wouldn&#39;t expect much
+        as they&#39;re far removed from their heyday during the PS3 generation.
       </p>
-      <p>
-        With rising development costs, general inflation, and the type of games
-        Media Molecule make not being commercially viable, It's far more likely
-        the studio will close before it releases a new game. Here's hoping I'm
-        wrong, as Sony needs a diverse portfolio of first-party games that Media
-        Molecule will bring.
+      <p className="mb-5 text-lg">
+        Dreams wasn&#39;t close to a commercial success, the vast majority of
+        Media Molecule&#39;s founders have left, and it&#39;s been reported that
+        they were extremely close to being shuttered in late 2023, instead of
+        London Studio.
       </p>
-    </>
+      <p className="mb-5 text-lg">
+        With rising development costs, smaller profit margins, and Media
+        Molecule&#39;s games not being commercially viable to a mainstream
+        audience, it&#39;s far more likely the studio will close before it
+        releases another game.
+      </p>
+      <p className="mb-5 text-lg">
+        For what it&#39;s worth, I hope I&#39;m wrong. Even if they don&#39;t
+        sell well, Sony needs the types of games Media Molecule makes to have a
+        diverse portfolio.
+      </p>
+      <Image
+        src={sackboy}
+        alt="Sackboy from LittleBigPlanet"
+        width={500}
+        height={500}
+        id="sackboy"
+        className="my-3 rounded-lg"
+      />
+      <label htmlFor="sackboy" className="block text-center">
+        LittleBigPlanet&#39;s mascot - Sackboy
+      </label>
+    </div>
   );
 }
 

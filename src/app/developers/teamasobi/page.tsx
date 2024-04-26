ac9fa9f3a8679d9944c0381b4be8deb69822c84b
acktreptow <acktreptow@gmail.com>
2024-04-26 23:33:26 +0100

    add near final text to asobi page

--- src/app/developers/teamasobi/page.tsx
+++ src/app/developers/teamasobi/page.tsx
@@ -13,18 +13,18 @@ function TeamAsobiPage() {
       </h2>
       <p className="mb-5 text-lg">
         Team Asobi is a Tokyo-based developer that technically, was formed in
-        2021. They were originally formed in 2012 as part of Japan Studio, but
-        became independent once Japan Studio was shut down in 2020.
+        2021. Asobi originally formed in 2012 as part of Japan Studio, but
+        became independent when Japan Studio closed in 2020.
       </p>
       <p className="mb-5 text-lg">
-        The developer are primarily known for the smaller-scoped games that
-        focus on new PlayStation hardware. This was seen with The PlayRoom to
-        show off the DualShock 4 and Astro Bot Rescue Mission to show off PSVR.
+        They&#39;re known for smaller-scoped games that focus on new PlayStation
+        hardware. For example, The PlayRoom to show off the DualShock 4 and
+        Astro Bot Rescue Mission for PSVR.
       </p>
       <p className="mb-8 text-lg">
-        Their most recent game is Astro's Playroom. A free PS5 launch title that
-        comes pre-installed. Its platforming was developer in a way to show off
-        the consoles new features like haptic feedback.
+        Their most recent game is Astro&#39;s Playroom. A free PS5 launch title
+        that comes pre-installed. Its gameplay was developed in a way to show
+        off the PS5&#39;s new features like haptic feedback.
       </p>
       <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
         <span className="border-b-2 border-gray-300">
@@ -32,23 +32,27 @@ function TeamAsobiPage() {
         </span>
       </h2>
       <p className="mb-5 text-lg">
-        Despite their last game releasing over 3 years ago, with PSVR2 releasing
-        during that time frame, we still haven't officially heard anything about
-        their next game.
+        Despite Astro&#39;s Playroom releasing over three years ago, we still
+        haven&#39;t officially heard anything about Asobi&#39;s next game.
       </p>
       <p className="mb-5 text-lg">
-        However, there are rumors that there next game will have a short
-        marketing cycle and will release towards the end of 2024. It will also
-        be slightly larger in scale, and playable on both the TV screen and
-        through PSVR2, taking advantage of both hardware.
+        Unofficially, there are rumors that it will release shortly after
+        announcement, at the end of 2024. It&#39;ll also be larger in scale, and
+        playable on TVs and PSVR2.
       </p>
       <p className="mb-5 text-lg">
-        Astro's Playroom was an incredible game that really showed off next-gen
-        hardware in a tight package. After all, it was the reason they survived
-        the closure of Japan Studio. With their next game also having double the
-        development time as their used to, it should be a great game that
-        hopefully showcases PSVR2 as the headset needs a win. Hopefully it is
-        true that it comes out this year too.
+        Astro&#39;s Playroom was an incredible game that even after hundreds of
+        PS5 games later, still has made the best use of the DualSense. It was
+        also the reason Asobi survived the closure of Japan Studio.
+      </p>
+      <p className="mb-5 text-lg">
+        With this next game also having double the development time, there&#39;s
+        every reason to believe it will be a great game that diversifies
+        Sony&#39;s portfolio.
+      </p>
+      <p className="mb-5 text-lg">
+        It hopefully also showcases PSVR2 well, because that VR headset
+        desperately needs a win.
       </p>
       <Image
         src={astroBot}

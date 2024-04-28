080d2e6a039de0c2c48b33959ee7044bbbb1f775
acktreptow <acktreptow@gmail.com>
2024-04-28 19:45:29 +0100

    add games section to naughty dog page

--- src/app/developers/naughtydog/page.tsx
+++ src/app/developers/naughtydog/page.tsx
@@ -51,11 +51,34 @@ function NaughtyDogPage() {
         A huge drop-off from previous output, although I&#39;m sure it will
         retain their premier quality.
       </p>
-      <p className="mb-5 text-lg">
+      <p className="mb-8 text-lg">
         Regardless, many first-party developers have already supplanted them
         this gen. Something unthinkable when TLOU released in 2013. Hopefully
         they return to their former glory for the PS6.
       </p>
+      <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
+        <span className="border-b-2 border-gray-300">PS5 Games</span>
+      </h2>
+      <div className="mb-5 grid gap-5">
+        {games
+          .filter((game) => game.developer === "Naughty Dog")
+          .map((game) => (
+            <div
+              key={game.id}
+              className="border-2 border-gray-200 rounded-lg shadow-md p-3"
+            >
+              <h3 className="text-xl font-bold text-center mb-1">
+                {game.name}
+              </h3>
+              <p className="text-lg">Genre: {game.genre}</p>
+              <p className="text-lg">Release Date: {game.releaseDate}</p>
+              <p className="text-lg">
+                Days Between Releases: {game.daysBetweenReleases}
+              </p>
+              <p className="text-lg">Metacritic Score: {game.metacritic}</p>
+            </div>
+          ))}
+      </div>
       <Image
         src={tlouFactions}
         alt="The Last of Us Factions"

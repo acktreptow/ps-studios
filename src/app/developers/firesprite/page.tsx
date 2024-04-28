c34075f92fae3e50374b7b45bbe08a652fb240d3
acktreptow <acktreptow@gmail.com>
2024-04-28 18:37:50 +0100

    update firesprite games section

--- src/app/developers/firesprite/page.tsx
+++ src/app/developers/firesprite/page.tsx
@@ -61,14 +61,19 @@ function FirespritePage() {
         {games
           .filter((game) => game.developer === "Firesprite")
           .map((game) => (
-            <div key={game.id} className="border p-3">
-              <h3 className="text-lg font-semibold text-center mb-1">
+            <div
+              key={game.id}
+              className="border-2 border-gray-200 rounded-lg shadow-md p-3"
+            >
+              <h3 className="text-xl font-bold text-center mb-1">
                 {game.name}
               </h3>
+              <p className="text-lg">Genre: {game.genre}</p>
               <p className="text-lg">Release Date: {game.releaseDate}</p>
               <p className="text-lg">
-                Days between releases: {game.daysBetweenReleases}
+                Days Between Releases: {game.daysBetweenReleases}
               </p>
+              <p className="text-lg">Metacritic Score: {game.metacritic}</p>
             </div>
           ))}
       </div>

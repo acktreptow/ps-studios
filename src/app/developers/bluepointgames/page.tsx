382b1f0621bf0aa46604c3179172aae1cf630bcc
acktreptow <acktreptow@gmail.com>
2024-04-28 19:23:59 +0100

    add games to bluepoint page

--- src/app/developers/bluepointgames/page.tsx
+++ src/app/developers/bluepointgames/page.tsx
@@ -44,13 +44,35 @@ function BluepointGamesPage() {
         not too surprising that development is taking longer than their previous
         games. I&#39;m also optimistic due to their excellent track record.
       </p>
-      <p className="mb-5 text-lg">
+      <p className="mb-8 text-lg">
         That outlook might change if we go another year without any news though.
         Four years is too long for Bluepoint to go without an announcement.
       </p>
       <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">Games</span>
+        <span className="border-b-2 border-gray-300">
+          Bluepoint&#39;s PS5 Games
+        </span>
       </h2>
+      <div className="mb-5 grid gap-5">
+        {games
+          .filter((game) => game.developer === "Bluepoint Games")
+          .map((game) => (
+            <div
+              key={game.id}
+              className="border-2 border-gray-200 rounded-lg shadow-md p-3"
+            >
+              <h3 className="text-xl font-bold text-center mb-1">
+                {game.name}
+              </h3>
+              <p className="text-lg">Genre: {game.genre}</p>
+              <p className="text-lg">Release Date: {game.releaseDate}</p>
+              <p className="text-lg">
+                Days Between Releases: {game.daysBetweenReleases}
+              </p>
+              <p className="text-lg">Metacritic Score: {game.metacritic}</p>
+            </div>
+          ))}
+      </div>
       <Image
         src={demonsSouls}
         alt="Demons's Souls cover art"

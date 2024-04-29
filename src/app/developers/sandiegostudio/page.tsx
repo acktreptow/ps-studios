4d4d171520c40e42db66ee0b4c415dd535f66d0d
acktreptow <acktreptow@gmail.com>
2024-04-29 01:30:43 +0100

    add css for non-mobile screens on san diego page

--- src/app/developers/sandiegostudio/page.tsx
+++ src/app/developers/sandiegostudio/page.tsx
@@ -4,7 +4,7 @@ import mlb24 from "../../../../public/images/mlb-24.png";
 
 function SanDiegoStudioPage() {
   return (
-    <div className="bg-white text-gray-700 p-10">
+    <div className="bg-white text-gray-700 p-10 container mx-auto">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         San Diego Studio
       </h1>
@@ -27,27 +27,46 @@ function SanDiegoStudioPage() {
         Clearly, turning the series platform was the better alternative to
         losing the license as the series has continued.
       </p>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">
-          My Analysis and Predictions
-        </span>
-      </h2>
-      <p className="mb-5 text-lg">
-        This ones easy to predict. SD Studio haven&#39;t developed a non-MLB
-        game for close to a decade. And with rising development costs coupled
-        with tighter profits for PlayStation, it&#39;s unlikely they would be
-        allowed to experiment with something new, even if they want to.
-      </p>
-      <p className="mb-8 text-lg">
-        We&#39;re also four games deep into the series being multiplatform and
-        nothing has materially changed. That was the one change that could have
-        altered MLB&#39;s trajectory, but didn&#39;t. It&#39;s therefore safe to
-        assume SD Studio will continue for the foreseeable future.
-      </p>
+      <div className="md:flex items-center">
+        <div>
+          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+            <span className="border-b-2 border-gray-300">
+              My Analysis and Predictions
+            </span>
+          </h2>
+          <p className="mb-5 text-lg">
+            This ones easy to predict. SD Studio haven&#39;t developed a non-MLB
+            game for close to a decade. And with rising development costs
+            coupled with tighter profits for PlayStation, it&#39;s unlikely they
+            would be allowed to experiment with something new, even if they want
+            to.
+          </p>
+          <p className="mb-8 text-lg">
+            We&#39;re also four games deep into the series being multiplatform
+            and nothing has materially changed. That was the one change that
+            could have altered MLB&#39;s trajectory, but didn&#39;t. It&#39;s
+            therefore safe to assume SD Studio will continue for the foreseeable
+            future.
+          </p>
+        </div>
+        <div>
+          <Image
+            src={mlb24}
+            alt="MLB The Show 24 promotional material"
+            width={500}
+            height={500}
+            id="mlb-24"
+            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+          />
+          <label htmlFor="mlb-24" className="block text-center mb-8">
+            The Show 24 promotional material
+          </label>
+        </div>
+      </div>
       <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
         <span className="border-b-2 border-gray-300">PS5 Games</span>
       </h2>
-      <div className="mb-5 grid gap-5">
+      <div className="mb-5 grid gap-5  sm:grid-cols-2">
         {games
           .filter((game) => game.developer === "San Diego Studio")
           .map((game) => (
@@ -67,17 +86,6 @@ function SanDiegoStudioPage() {
             </div>
           ))}
       </div>
-      <Image
-        src={mlb24}
-        alt="MLB The Show 24 promotional material"
-        width={500}
-        height={500}
-        id="mlb-24"
-        className="my-3 rounded-lg"
-      />
-      <label htmlFor="mlb-24" className="block text-center mb-8">
-        MLB The Show 24 promotional material
-      </label>
     </div>
   );
 }

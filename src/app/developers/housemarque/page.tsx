12f3a85ec6a8edb19f0ed8a764d827a03e988ffe
acktreptow <acktreptow@gmail.com>
2024-04-30 22:25:31 +0100

    add non-mobile css to housemarque page

--- src/app/developers/housemarque/page.tsx
+++ src/app/developers/housemarque/page.tsx
@@ -4,7 +4,7 @@ import seleneVassos from "../../../../public/images/selene-vassos.png";
 
 function HousemarquePage() {
   return (
-    <div className="bg-white text-gray-700 p-10">
+    <div className="bg-white text-gray-700 p-10 container mx-auto">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Housemarque
       </h1>
@@ -28,30 +28,48 @@ function HousemarquePage() {
         Clearly, Sony were happy with this pivot as they purchased Housemarque
         two months after Returnal released.
       </p>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">
-          My Analysis and Predictions
-        </span>
-      </h2>
-      <p className="mb-5 text-lg">
-        Returnal is Housemarque&#39;s only PS5 title. Yet despite releasing
-        three years ago, we know nothing about the next game. Nonetheless,
-        Returnal&#39;s quality is the reason to still be excited.
-      </p>
-      <p className="mb-5 text-lg">
-        With Returnal, Housemarque proved they could take the elements that made
-        their older, smaller-scoped games critically acclaimed, and repackage
-        them into a more mainstream title.
-      </p>
-      <p className="mb-8 text-lg">
-        With this experience under their belt, plus the backing of Sony as an
-        official first-party developer, their next game should fare even better.
-        It has been 3 years though, so we should (hopefully) hear about it soon.
-      </p>
+      <div className="md:flex items-center">
+        <div>
+          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+            <span className="border-b-2 border-gray-300">
+              My Analysis and Predictions
+            </span>
+          </h2>
+          <p className="mb-5 text-lg">
+            Returnal is Housemarque&#39;s only PS5 title. Yet despite releasing
+            three years ago, we know nothing about the next game. Nonetheless,
+            Returnal&#39;s quality is the reason to still be excited.
+          </p>
+          <p className="mb-5 text-lg">
+            With Returnal, Housemarque proved they could take the elements that
+            made their older, smaller-scoped games critically acclaimed, and
+            repackage them into a more mainstream title.
+          </p>
+          <p className="mb-8 text-lg">
+            With this experience under their belt, plus the backing of Sony as
+            an official first-party developer, their next game should fare even
+            better. It has been 3 years though, so we should (hopefully) hear
+            about it soon.
+          </p>
+        </div>
+        <div>
+          <Image
+            src={seleneVassos}
+            alt="Returnal's Selene Vassos"
+            width={500}
+            height={500}
+            id="selene-vassos"
+            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+          />
+          <label htmlFor="selene-vassos" className="block text-center mb-8">
+            Returnal&#39;s main character, Selene Vassos
+          </label>
+        </div>
+      </div>
       <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
         <span className="border-b-2 border-gray-300">PS5 Games</span>
       </h2>
-      <div className="mb-5 grid gap-5">
+      <div className="mb-5 grid gap-5 sm:grid-cols-2">
         {games
           .filter((game) => game.developer === "Housemarque")
           .map((game) => (
@@ -71,17 +89,6 @@ function HousemarquePage() {
             </div>
           ))}
       </div>
-      <Image
-        src={seleneVassos}
-        alt="Returnal's Selene Vassos"
-        width={500}
-        height={500}
-        id="selen-vassos"
-        className="my-3 rounded-lg"
-      />
-      <label htmlFor="selen-vassos" className="block text-center">
-        Returnal&#39;s main character, Selene Vassos
-      </label>
     </div>
   );
 }

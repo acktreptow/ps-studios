5bff81abbb57d2dfebdb74736af20483c46fba03
acktreptow <acktreptow@gmail.com>
2024-05-03 20:12:57 +0100

    add non-mobile css to media molecule page

--- src/app/developers/mediamolecule/page.tsx
+++ src/app/developers/mediamolecule/page.tsx
@@ -3,7 +3,7 @@ import sackboy from "../../../../public/images/sackboy.png";
 
 function MediaMoleculePage() {
   return (
-    <div className="bg-white text-gray-700 p-10">
+    <div className="bg-white text-gray-700 p-10 container mx-auto">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Media Molecule
       </h1>
@@ -21,43 +21,50 @@ function MediaMoleculePage() {
         we don&#39;t even have a name, let alone gameplay, despite Dreams
         releasing 4 years ago.
       </p>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">
-          My Analysis and Predictions
-        </span>
-      </h2>
-      <p className="mb-5 text-lg">
-        Whatever Media Molecule&#39;s next game is, I wouldn&#39;t expect much
-        as they&#39;re far removed from their heyday during the PS3 generation.
-      </p>
-      <p className="mb-5 text-lg">
-        Dreams wasn&#39;t close to a commercial success, the vast majority of
-        Media Molecule&#39;s founders have left, and it&#39;s been reported that
-        they were extremely close to being shuttered in late 2023, instead of
-        London Studio.
-      </p>
-      <p className="mb-5 text-lg">
-        With rising development costs, smaller profit margins, and Media
-        Molecule&#39;s games not being commercially viable to a mainstream
-        audience, it&#39;s far more likely the studio will close before it
-        releases another game.
-      </p>
-      <p className="mb-5 text-lg">
-        For what it&#39;s worth, I hope I&#39;m wrong. Even if they don&#39;t
-        sell well, Sony needs the types of games Media Molecule makes to have a
-        diverse portfolio.
-      </p>
-      <Image
-        src={sackboy}
-        alt="Sackboy from LittleBigPlanet"
-        width={500}
-        height={500}
-        id="sackboy"
-        className="my-3 rounded-lg"
-      />
-      <label htmlFor="sackboy" className="block text-center">
-        LittleBigPlanet&#39;s mascot - Sackboy
-      </label>
+      <div className="md:flex items-center">
+        <div>
+          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+            <span className="border-b-2 border-gray-300">
+              My Analysis and Predictions
+            </span>
+          </h2>
+          <p className="mb-5 text-lg">
+            Whatever Media Molecule&#39;s next game is, I wouldn&#39;t expect
+            much as they&#39;re far removed from their heyday during the PS3
+            generation.
+          </p>
+          <p className="mb-5 text-lg">
+            Dreams wasn&#39;t close to a commercial success, the vast majority
+            of Media Molecule&#39;s founders have left, and it&#39;s been
+            reported that they were extremely close to being shuttered in late
+            2023, instead of London Studio.
+          </p>
+          <p className="mb-5 text-lg">
+            With rising development costs, smaller profit margins, and Media
+            Molecule&#39;s games not being commercially viable to a mainstream
+            audience, it&#39;s far more likely the studio will close before it
+            releases another game.
+          </p>
+          <p className="mb-8 text-lg">
+            For what it&#39;s worth, I hope I&#39;m wrong. Even if they
+            don&#39;t sell well, Sony needs the types of games Media Molecule
+            makes to have a diverse portfolio.
+          </p>
+        </div>
+        <div>
+          <Image
+            src={sackboy}
+            alt="Sackboy from LittleBigPlanet"
+            width={500}
+            height={500}
+            id="sackboy"
+            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+          />
+          <label htmlFor="sackboy" className="block text-center">
+            LittleBigPlanet&#39;s mascot - Sackboy
+          </label>
+        </div>
+      </div>
     </div>
   );
 }

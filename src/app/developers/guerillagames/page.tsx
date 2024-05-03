ee903b4175c8e38e8a8e02170b850277e6109b88
acktreptow <acktreptow@gmail.com>
2024-05-03 19:51:11 +0100

    add non-mobile css to guerilla page

--- src/app/developers/guerillagames/page.tsx
+++ src/app/developers/guerillagames/page.tsx
@@ -4,7 +4,7 @@ import burningShores from "../../../../public/images/burning-shores.png";
 
 function GuerillaGamesPage() {
   return (
-    <div className="bg-white text-gray-700 p-10">
+    <div className="bg-white text-gray-700 p-10 container mx-auto">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Guerilla Games
       </h1>
@@ -30,42 +30,61 @@ function GuerillaGamesPage() {
         Since the 2023 DLC, Guerilla have been silent. We don&#39;t even have a
         name for their next project.
       </p>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">
-          My Analysis and Predictions
-        </span>
-      </h2>
-      <p className="mb-5 text-lg">
-        Despite no concrete information, I predict Guerilla&#39;s next game is
-        Horizon 3 for the following reasons:
-      </p>
-      <ul className="list-disc pl-5 mb-5 text-lg">
-        <li>
-          Horizon FW&#39;s ending sets up a sequel that serves as the final act
-          of a trilogy.
-        </li>
-        <li>Netflix is working on a live action Horizon TV series.</li>
-        <li>
-          A remake of the first Horizon is likely being developed at Nixxes.
-        </li>
-        <li>
-          A Horizon online MMO is almost certainly being worked on at NCSoft.
-        </li>
-      </ul>
-      <p className="mb-5 text-lg">
-        Clearly, Sony are bullish on Horizon with so many projects in
-        development. It would therefore be nonsensical for Guerilla to move on
-        right now.
-      </p>
-      <p className="mb-8 text-lg">
-        You can expect &#39;Horizon 3&#39; in roughly 2027 that plays similar to
-        the first two. It&#39;s only been two years since Forbidden West though,
-        so don&#39;t expect to see anything until 2025 at the earliest.
-      </p>
+      <div className="md:flex items-center">
+        <div>
+          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+            <span className="border-b-2 border-gray-300">
+              My Analysis and Predictions
+            </span>
+          </h2>
+          <p className="mb-5 text-lg">
+            Despite no concrete information, I predict Guerilla&#39;s next game
+            is Horizon 3 for the following reasons:
+          </p>
+          <ul className="list-disc pl-5 mb-5 text-lg sm:pl-10">
+            <li>
+              Horizon FW&#39;s ending sets up a sequel that serves as the final
+              act of a trilogy.
+            </li>
+            <li>Netflix is working on a live action Horizon TV series.</li>
+            <li>
+              A remake of the first Horizon is likely being developed at Nixxes.
+            </li>
+            <li>
+              A Horizon online MMO is almost certainly being worked on at
+              NCSoft.
+            </li>
+          </ul>
+          <p className="mb-5 text-lg">
+            Clearly, Sony are bullish on Horizon with so many projects in
+            development. It would therefore be nonsensical for Guerilla to move
+            on right now.
+          </p>
+          <p className="mb-8 text-lg">
+            You can expect &#39;Horizon 3&#39; in roughly 2027 that plays
+            similar to the first two. It&#39;s only been two years since
+            Forbidden West though, so don&#39;t expect to see anything until
+            2025 at the earliest.
+          </p>
+        </div>
+        <div>
+          <Image
+            src={burningShores}
+            alt="Horizon Burning Shores key art"
+            width={500}
+            height={500}
+            id="burning-shores"
+            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+          />
+          <label htmlFor="burning-shores" className="block text-center mb-8">
+            Key art for Horizon FW&#39;s DLC: Burning Shores
+          </label>
+        </div>
+      </div>
       <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
         <span className="border-b-2 border-gray-300">PS5 Games</span>
       </h2>
-      <div className="mb-5 grid gap-5">
+      <div className="mb-5 grid gap-5 sm:grid-cols-2">
         {games
           .filter((game) => game.developer === "Guerrilla Games")
           .map((game) => (
@@ -85,17 +104,6 @@ function GuerillaGamesPage() {
             </div>
           ))}
       </div>
-      <Image
-        src={burningShores}
-        alt="Horizon Burning Shores key art"
-        width={500}
-        height={500}
-        id="burning-shores"
-        className="my-3 rounded-lg"
-      />
-      <label htmlFor="burning-shores" className="block text-center">
-        Key art for Horizon FW&#39;s DLC: Burning Shores
-      </label>
     </div>
   );
 }

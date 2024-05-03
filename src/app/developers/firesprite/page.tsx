d6adfdb9768caff03868159b469a0576106fa632
acktreptow <acktreptow@gmail.com>
2024-05-03 19:39:20 +0100

    add non-mobile css to firesprite page

--- src/app/developers/firesprite/page.tsx
+++ src/app/developers/firesprite/page.tsx
@@ -4,7 +4,7 @@ import horizonCoTM from "../../../../public/images/horizon-call-of-the-mountain.
 
 function FirespritePage() {
   return (
-    <div className="bg-white text-gray-700 p-10">
+    <div className="bg-white text-gray-700 p-10 container mx-auto">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Firesprite
       </h1>
@@ -26,36 +26,53 @@ function FirespritePage() {
         Persistence and the flagship PSVR2 launch title Horizon Call of the
         Mountain. Neither were particularly well received.
       </p>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">
-          My Analysis and Predictions
-        </span>
-      </h2>
-      <p className="mb-5 text-lg">
-        Firesprite hasn&#39;t announced anything since Horizon CoTM shipped 14
-        months ago. Unfortunately this is because of the studio&#39;s dire
-        situation, post-acquisition.
-      </p>
-      <p className="mb-5 text-lg">
-        Crunch was apparently necessary to finish Horizon. Additionally, all but
-        one founder and many other staff left once retention bonuses were paid
-        out.
-      </p>
-      <p className="mb-5 text-lg">
-        Finally, further layoffs took place a year later and it was reported
-        that Firesprite&#39;s unrevealed Twisted Metal live service game had
-        been cancelled.
-      </p>
-      <p className="mb-8 text-lg">
-        Considering this toxic environment, plus the fact that their games
-        weren&#39;t great beforehand, I don&#39;t see anything good coming out
-        of Firesprite soon. Even if early leaks of a potential horror game do
-        sound promising in a vacuum.
-      </p>
+      <div className="md:flex items-center">
+        <div>
+          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+            <span className="border-b-2 border-gray-300">
+              My Analysis and Predictions
+            </span>
+          </h2>
+          <p className="mb-5 text-lg">
+            Firesprite hasn&#39;t announced anything since Horizon CoTM shipped
+            14 months ago. Unfortunately this is because of the studio&#39;s
+            dire situation, post-acquisition.
+          </p>
+          <p className="mb-5 text-lg">
+            Crunch was apparently necessary to finish Horizon. Additionally, all
+            but one founder and many other staff left once retention bonuses
+            were paid out.
+          </p>
+          <p className="mb-5 text-lg">
+            Finally, further layoffs took place a year later and it was reported
+            that Firesprite&#39;s unrevealed Twisted Metal live service game had
+            been cancelled.
+          </p>
+          <p className="mb-8 text-lg">
+            Considering this toxic environment, plus the fact that their games
+            weren&#39;t great beforehand, I don&#39;t see anything good coming
+            out of Firesprite soon. Even if early leaks of a potential horror
+            game do sound promising in a vacuum.
+          </p>
+        </div>
+        <div>
+          <Image
+            src={horizonCoTM}
+            alt="Horizon Call of the MMountain gameplay screenshot"
+            width={500}
+            height={500}
+            id="horizon-cotm"
+            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+          />
+          <label htmlFor="horizon-cotm" className="block text-center mb-8">
+            Horizon CoTM on PSVR2
+          </label>
+        </div>
+      </div>
       <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
         <span className="border-b-2 border-gray-300">PS5 Games</span>
       </h2>
-      <div className="mb-5 grid gap-5">
+      <div className="mb-5 grid gap-5 sm:grid-cols-2">
         {games
           .filter((game) => game.developer === "Firesprite")
           .map((game) => (
@@ -75,17 +92,6 @@ function FirespritePage() {
             </div>
           ))}
       </div>
-      <Image
-        src={horizonCoTM}
-        alt="Horizon Call of the MMountain gameplay screenshot"
-        width={500}
-        height={500}
-        id="horizon-cotm"
-        className="my-3 rounded-lg"
-      />
-      <label htmlFor="horizon-cotm" className="block text-center">
-        Horizon CoTM on PSVR2
-      </label>
     </div>
   );
 }

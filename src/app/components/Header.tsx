61d37cd8a9d9558860591ea0b41465943abdbf37
acktreptow <acktreptow@gmail.com>
2024-05-06 23:49:12 +0100

    update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
 import Image from "next/image";
-import psStudiosLogo from "../../../public/images/ps-studios-logo.svg";
+import psStudiosLogo from "../../../public/images/ps-studios-logo.png";
 
 function Header() {
   return (
-    <header className="bg-blue-400 flex px-6 py-4 justify-between items-center container mx-auto">
-      <h1>Sony First Party</h1>
-      <Image src={psStudiosLogo} alt="PS Studios Logo" className="w-8" />
+    <header className="bg-playstation text-white flex px-10  justify-between items-center container mx-auto">
+      <h1 className="hover:text-gray-200">
+        <Link href="/">PS5 First-Party Fiesta</Link>
+      </h1>
+      <Image src={psStudiosLogo} alt="PS Studios Logo" className="w-40" />
       <ul className="flex gap-2">
-        <li>
-          <Link href="/">Home</Link>
+        <li className="hover:text-gray-200">
+          <Link href="/about">About</Link>
         </li>
-        <li>
+        <li className="hover:text-gray-200">
           <Link href="/developers">Developers</Link>
+        </li>
+        <li className="hover:text-gray-200">
+          <Link href="/ps5gameslist">PS5 Games</Link>
         </li>
       </ul>
     </header>

1a3c0b4d09b269f8d853a554f2c04106497ed1fd
acktreptow <acktreptow@gmail.com>
2024-05-08 03:31:03 +0100

    add flex-grow

--- src/app/developers/housemarque/page.tsx
+++ src/app/developers/housemarque/page.tsx
@@ -4,7 +4,7 @@ import seleneVassos from "../../../../public/images/selene-vassos.png";
 
 function HousemarquePage() {
   return (
-    <div className="bg-white text-gray-700 p-10 container mx-auto">
+    <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Housemarque
       </h1>

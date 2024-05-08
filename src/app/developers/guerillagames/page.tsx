72fccdffbc13db957ced5e57431ad727d82bc095
acktreptow <acktreptow@gmail.com>
2024-05-08 03:27:59 +0100

    add flex-grow

--- src/app/developers/guerillagames/page.tsx
+++ src/app/developers/guerillagames/page.tsx
@@ -4,7 +4,7 @@ import burningShores from "../../../../public/images/burning-shores.png";
 
 function GuerillaGamesPage() {
   return (
-    <div className="bg-white text-gray-700 p-10 container mx-auto">
+    <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Guerilla Games
       </h1>
--- src/app/developers/havenstudios/page.tsx
+++ src/app/developers/havenstudios/page.tsx
@@ -5,7 +5,7 @@ import fairGames from "../../../../public/images/fairgames.png";
 
 function HavenStudiosPage() {
   return (
-    <div className="bg-white text-gray-700 p-10 container mx-auto">
+    <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Haven Studios
       </h1>

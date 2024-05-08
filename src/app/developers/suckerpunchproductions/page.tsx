65da5c88372ac70ca5b415796d77d8eef74b2b14
acktreptow <acktreptow@gmail.com>
2024-05-08 03:38:59 +0100

    add flex-grow

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -4,7 +4,7 @@ import gotDC from "../../../../public/images/got-dc.png";
 
 function SuckerPunchProductionsPage() {
   return (
-    <div className="bg-white text-gray-700 p-10 container mx-auto">
+    <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Sucker Punch Productions
       </h1>

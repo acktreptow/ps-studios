c436b404e688109dc26110e98d043d705c646c5c
acktreptow <acktreptow@gmail.com>
2024-05-10 03:12:40 +0100

    start second section

--- src/app/devstats/page.tsx
+++ src/app/devstats/page.tsx
@@ -12,12 +12,12 @@ function DevStatsPage() {
       <p className="mb-5 text-lg">
         Here are some stats for PS Studios over the PS5 generation.
       </p>
-      <p className="mb-8 text-lg">
+      <p className="mb-5 text-lg">
         There&#39;s 22 first-party developers under the PS Studios umbrella. The
         following list are their names and the number of{" "}
         <span className="font-bold">released</span> PS5 games:
       </p>
-      <ul className="list-disc pl-5 mb-5 text-lg md:pl-7 lg:pl-10">
+      <ul className="list-disc pl-5 mb-8 text-lg md:pl-7 lg:pl-10">
         {developers
           .filter(
             (developer) =>
@@ -26,7 +26,8 @@ function DevStatsPage() {
           )
           .map((developer) => (
             <li key={developer.id} className="mb-1">
-              {developer.name}: {developer.ps5Games}
+              <span className="font-semibold">{developer.name}</span>:{" "}
+              {developer.ps5Games}
             </li>
           ))}
       </ul>
@@ -36,34 +37,9 @@ function DevStatsPage() {
           <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
             <span className="border-b-2 border-gray-300">Other Stats</span>
           </h2>
-          <p className="mb-5 text-lg">
-            Despite Days Gone releasing 5 years ago, we still know very little
-            about their next title. But due to internal leaks and some
-            investigative-reporting, here&#39;s what we do know:
-          </p>
           <ul className="list-disc pl-5 mb-5 text-lg md:pl-7 lg:pl-10">
-            <li>
-              Bend pitched Days Gone 2, which Sony rejected due to poor critical
-              reception of Days Gone.
-            </li>
-            <li>
-              A power struggle occurred in 2020 resulting in the writer and
-              creative director leaving.
-            </li>
-            <li>It will be a new IP under new leadership.</li>
-            <li>It will be a live service game.</li>
+            <li>Total PS5 games released: 29</li>
           </ul>
-          <p className="mb-5 text-lg">
-            The vast majority of Days Gone&#39;s criticisms could be attributed
-            to those who lost the power struggle and left Bend. Between that and
-            the fact that it&#39;s already been 5 years, I believe that
-            regardless of the move to live service, Bend&#39;s next game will
-            fare better than Days Gone. At least critically.
-          </p>
-          <p className="mb-8 text-lg">
-            We should expect to see something soon as Bend have gone the longest
-            out of all PS Studios without releasing a game.
-          </p>
         </div>
       </div>
     </div>

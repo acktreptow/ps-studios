42eb584b2f9fe55c920f9e18a55045c466b7dbe9
acktreptow <acktreptow@gmail.com>
2024-05-11 00:05:20 +0100

    split out stats

--- src/app/devstats/page.tsx
+++ src/app/devstats/page.tsx
@@ -34,11 +34,31 @@ function DevStatsPage() {
 
       <div className="lg:flex items-center">
         <div>
-          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
+          <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
             <span className="border-b-2 border-gray-300">Other Stats</span>
           </h2>
+          <h3 className="mb-1 text-gray-800 text-xl font-semibold pb-1">
+            Game Stats:
+          </h3>
           <ul className="list-disc pl-5 mb-5 text-lg md:pl-7 lg:pl-10">
             <li>Total PS5 games released: 29</li>
+            <li>Total 90+ PS5 games on Metacritic: 4</li>
+            <li>Total 80+ PS5 games on Metacritic: 21</li>
+            <li>Games released in 2020: 6</li>
+            <li>Games released in 2021: 8</li>
+            <li>Games released in 2022: 6</li>
+            <li>Games released in 2023: 4</li>
+            <li>Games released in 2024: 5</li>
+            <li>Unreleased announced PS5 games: 6</li>
+          </ul>
+          <h3 className="mb-1 text-gray-800 text-xl font-semibold pb-1">
+            Studio Stats:
+          </h3>
+          <ul className="list-disc pl-5 mb-5 text-lg md:pl-7 lg:pl-10">
+            <li>PS Studios in NA: 13</li>
+            <li>PS Studios in EU: 7</li>
+            <li>PS Studios in JP: 2</li>
+            <li>PS Studios acquired in PS5 generation: 10</li>
           </ul>
         </div>
       </div>

98af71af2cf4827301a0739b29c411c8d700108e
acktreptow <acktreptow@gmail.com>
2024-05-13 03:33:29 +0100

    added sort functionality

--- src/app/ps5games/page.tsx
+++ src/app/ps5games/page.tsx
@@ -1,13 +1,34 @@
+"use client";
+import { useState } from "react";
 import games from "../data/games.json";
 
 function PS5Games() {
+  const [metacritic, setMetacritic] = useState(false);
+
   return (
     <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
       <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
         Every PlayStation Studios PS5 Game
       </h1>
+      <button
+        onClick={() => setMetacritic(false)}
+        className="mx-auto border p-4 mb-8 bg-playstation text-white rounded-full"
+      >
+        Sort by Release Date
+      </button>
+      <button
+        onClick={() => setMetacritic(true)}
+        className="mx-auto border p-4 mb-8 bg-playstation text-white rounded-full"
+      >
+        Sort by Metacritic score
+      </button>
       <div className="mb-5 grid gap-5 sm:grid-cols-2 ">
         {games
+          .sort((a, b) =>
+            metacritic
+              ? a.metacritic - b.metacritic
+              : new Date(a.releaseDate) - new Date(b.releaseDate)
+          )
           .filter((game) => game.developer !== "Nixxes Software")
           .map((game) => (
             <div

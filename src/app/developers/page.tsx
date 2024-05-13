6b12687f323bcdd6e326165b3e449b06af0e4ada
acktreptow <acktreptow@gmail.com>
2024-05-13 20:46:20 +0100

    implemented filter and map

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,3 +1,4 @@
+import developers from "../data/developers.json";
 import Link from "next/link";
 
 function DevelopersPage() {
@@ -7,72 +8,17 @@ function DevelopersPage() {
         PlayStation Developers
       </h1>
       <ul className="text-xl leading-8 md:text-2xl md:leading-10 ">
-        <Link href="/developers/bendstudio">
-          <li>Bend Studio</li>
-        </Link>
-        <Link href="/developers/bluepointgames">
-          <li>Bluepoint Games</li>
-        </Link>
-        <Link href="/developers/bungie">
-          <li>Bungie</li>
-        </Link>
-        <Link href="/developers/fabrikgames">
-          <li>Fabrik Games</li>
-        </Link>
-        <Link href="/developers/firesprite">
-          <li>Firesprite</li>
-        </Link>
-        <Link href="/developers/firewalkstudios">
-          <li>Firewalk Studios</li>
-        </Link>
-        <Link href="/developers/guerillagames">
-          <li>Guerilla Games</li>
-        </Link>
-        <Link href="/developers/havenstudios">
-          <li>Haven Studios</li>
-        </Link>
-        <Link href="/developers/housemarque">
-          <li>Housemarque</li>
-        </Link>
-        <Link href="/developers/insomniacgames">
-          <li>Insomniac Games</li>
-        </Link>
-        <Link href="/developers/mediamolecule">
-          <li>Media Molecule</li>
-        </Link>
-        <Link href="/developers/naughtydog">
-          <li>Naughty Dog</li>
-        </Link>
-        <Link href="/developers/neonkoi">
-          <li>Neon Koi</li>
-        </Link>
-        <Link href="/developers/nixxessoftware">
-          <li>Nixxes Software</li>
-        </Link>
-        <Link href="/developers/polyphonydigital">
-          <li>Polyphony Digital</li>
-        </Link>
-        <Link href="/developers/sandiegostudio">
-          <li>San Diego Studio</li>
-        </Link>
-        <Link href="/developers/sanmateostudio">
-          <li>San Mateo Studio</li>
-        </Link>
-        <Link href="/developers/santamonicastudio">
-          <li>Santa Monica Studio</li>
-        </Link>
-        <Link href="/developers/suckerpunchproductions">
-          <li>Sucker Punch Productions</li>
-        </Link>
-        <Link href="/developers/teamasobi">
-          <li>Team Asobi</li>
-        </Link>
-        <Link href="/developers/valkyrieentertainment">
-          <li>Valkyrie Entertainment</li>
-        </Link>
-        <Link href="/developers/xdev">
-          <li>XDev</li>
-        </Link>
+        {developers
+          .filter(
+            (developer) =>
+              developer.name !== "PlayStation Logo" &&
+              developer.name !== "PS5 Logo"
+          )
+          .map((developer) => (
+            <Link key={developer.id} href={`developers/${developer.urlPath}`}>
+              <li>{developer.name}</li>
+            </Link>
+          ))}
       </ul>
     </div>
   );

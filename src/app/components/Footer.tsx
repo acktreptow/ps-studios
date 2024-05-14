0629874e0338ffe01cbebb3b5d6341ec1d1daa5c
acktreptow <acktreptow@gmail.com>
2024-05-14 18:44:25 +0100

    add large screens css

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer className="container bg-playstation text-white text-center py-4 mx-auto">
-      <p className="font-semibold">PS5 Studios All Rights Reserved © 2024</p>
+      <p className="font-semibold lg:text-lg">
+        PS5 Studios All Rights Reserved © 2024
+      </p>
     </footer>
   );
 }

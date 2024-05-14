3ebce071bb251d868ddd4c1d44ea8e1f62d1cdce
acktreptow <acktreptow@gmail.com>
2024-05-14 03:17:25 +0100

    add large screens css

--- src/app/developers/havenstudios/page.tsx
+++ src/app/developers/havenstudios/page.tsx
@@ -6,7 +6,7 @@ import fairGames from "../../../../public/images/fairgames.png";
 function HavenStudiosPage() {
   return (
     <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
-      <h1 className="text-4xl text-center font-bold mb-10 text-gray-950">
+      <h1 className="text-4xl text-center font-bold mb-10 text-gray-950 tracking-wide md:leading-relaxed">
         Haven Studios
       </h1>
       <div className="lg:flex items-center">
@@ -14,18 +14,18 @@ function HavenStudiosPage() {
           <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
             <span className="border-b-2 border-gray-300">History</span>
           </h2>
-          <p className="mb-5 text-lg">
+          <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
             This Montreal-based developer was founded in 2021 by Jade Raymond -
             one of the creators of Assassin&#39;s Creed. Raymond was at Google,
             but left once they stopped supporting their cloud-based console
             &#39;Stadia&#39;.
           </p>
-          <p className="mb-5 text-lg">
+          <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
             Raymond took this cloud infrastructure knowledge to Haven, which
             caught the attention of Sony. They immediately started funding their
             first game and acquired Haven a year later.
           </p>
-          <p className="mb-8 text-lg">
+          <p className="mb-8 text-lg lg:text-xl lg:tracking-wide">
             This game turned out to be FairGame$. A live service competitive
             heist shooter that will launch on PS5 and PC. It currently has no
             release date.
@@ -38,7 +38,7 @@ function HavenStudiosPage() {
             width={500}
             height={500}
             id="jade-raymond"
-            className="my-3 rounded-lg md:max-w-xs lg:ml-5"
+            className="my-3 rounded-lg md:ml-5 md:max-w-xs lg:max-w-sm"
           />
           <label
             htmlFor="jade-raymond"
@@ -55,13 +55,13 @@ function HavenStudiosPage() {
               My Analysis & Predictions
             </span>
           </h2>
-          <p className="mb-5 text-lg">
+          <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
             We haven&#39;t seen Fairgame$ since the first trailer. Haven is also
             a new studio, and the first to emphasize the cloud. It&#39;s
             therefore hard to predict how Fairgame$ will do, but I&#39;m
             optimistic.
           </p>
-          <p className="mb-8 text-lg">
+          <p className="mb-8 text-lg lg:text-xl lg:tracking-wide">
             Mark Cerny, architect of the PS5, has taken an interest in Haven and
             is assisting with Fairgame$ and general R&D. Between his interest
             and Sony investing so quickly, I&#39;m confident.
@@ -74,7 +74,7 @@ function HavenStudiosPage() {
             width={500}
             height={500}
             id="fairgames"
-            className="my-3 rounded-lg md:mx-auto md:max-w-xs lg:max-w-sm md:ml-5"
+            className="my-3 rounded-lg md:ml-5 md:max-w-xs lg:max-w-md"
           />
           <label htmlFor="fairgames" className="block text-center mb-8">
             Fairgame$ only has a CGI trailer
@@ -92,15 +92,21 @@ function HavenStudiosPage() {
               key={game.id}
               className="border-2 border-gray-200 rounded-lg shadow-md p-3"
             >
-              <h3 className="text-xl font-bold text-center mb-1">
+              <h3 className="text-xl font-bold text-center mb-1 lg:text-2xl lg:mb-3">
                 {game.name}
               </h3>
-              <p className="text-lg">Genre: {game.genre}</p>
-              <p className="text-lg">Release Date: {game.releaseDate}</p>
-              <p className="text-lg">
-                Days Between Releases: {game.daysBetweenReleases}
+              <p className="text-lg lg:text-xl lg:tracking-wide">
+                Genre: {game.genre}
               </p>
-              <p className="text-lg">Metacritic Score: {game.metacritic}</p>
+              <p className="text-lg lg:text-xl lg:tracking-wide">
+                Release Date: {game.releaseDate}
+              </p>
+              <p className="text-lg lg:text-xl lg:tracking-wide">
+                Days Since Their Last Release: {game.daysBetweenReleases}
+              </p>
+              <p className="text-lg lg:text-xl lg:tracking-wide">
+                Metacritic Score: {game.metacritic}
+              </p>
             </div>
           ))}
       </div>

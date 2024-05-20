df07f25f54ce81fd444bca6e00b4f237504d57f1
acktreptow <acktreptow@gmail.com>
2024-05-20 22:42:23 +0100

    add game type

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import games from "../../data/games.json";
+import gamesData from "../../data/games.json";
 import Image from "next/image";
 import gotDC from "../../../../public/images/dev-pages/got-dc.png";
 
@@ -8,6 +8,18 @@ export const metadata: Metadata = {
   description:
     "The history and future of Sucker Punch, a first-party developer for PlayStation Studios.",
 };
+
+type Game = {
+  developer: string;
+  name: string;
+  id: string;
+  genre: string;
+  releaseDate: string;
+  daysBetweenReleases: number | string;
+  metacritic: number | string;
+};
+
+const games: Game[] = gamesData;
 
 function SuckerPunchProductionsPage(): JSX.Element {
   return (
@@ -75,8 +87,8 @@ function SuckerPunchProductionsPage(): JSX.Element {
       </h2>
       <div className="mb-5 grid gap-5 sm:grid-cols-2">
         {games
-          .filter((game) => game.developer === "Sucker Punch Productions")
-          .map((game) => (
+          .filter((game: Game) => game.developer === "Sucker Punch Productions")
+          .map((game: Game) => (
             <div
               key={game.id}
               className="border-2 border-gray-200 rounded-lg shadow-md p-3"

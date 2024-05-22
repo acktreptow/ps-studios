be8aeae972612461845478062ab2f017b3cb833c
acktreptow <acktreptow@gmail.com>
2024-05-22 19:19:56 +0100

    add components to sucker punch page

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -1,7 +1,14 @@
 import { Metadata } from "next";
 import gamesData from "../../data/games.json";
+import { Game } from "../../types/Game";
 import Image from "next/image";
 import gotDC from "../../../../public/images/dev-pages/got-dc.png";
+import Container from "../../components/Container";
+import DeveloperTitle from "../../components/DeveloperTitle";
+import DeveloperSubTitle from "../../components/DeveloperSubTitle";
+import Paragraph from "../../components/Paragraph";
+import ImageSection from "../../components/ImageSection";
+import GamesSection from "../../components/GamesSection";
 
 export const metadata: Metadata = {
   title: "Sucker Punch",
@@ -9,64 +16,46 @@ export const metadata: Metadata = {
     "The history and future of Sucker Punch, a first-party developer for PlayStation Studios.",
 };
 
-type Game = {
-  developer: string;
-  name: string;
-  id: string;
-  genre: string;
-  releaseDate: string;
-  daysBetweenReleases: number | string;
-  metacritic: number | string;
-};
-
-const games: Game[] = gamesData;
+const gamesArray: Game[] = gamesData;
 
 function SuckerPunchProductionsPage(): JSX.Element {
   return (
-    <div className="bg-white text-gray-700 p-10 container mx-auto flex-grow">
-      <h1 className="text-4xl text-center font-bold mb-10 text-gray-950 tracking-wide md:leading-relaxed">
-        Sucker Punch Productions
-      </h1>
-      <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">History</span>
-      </h2>
-      <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
+    <Container>
+      <DeveloperTitle title="Sucker Punch Productions" />
+      <DeveloperSubTitle subtitle="History" />
+      <Paragraph>
         Sucker Punch are a Washington-based developer that was founded in 1997,
         initially to work on an N64 game called Rocket: Robot on Wheels.
-      </p>
-      <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
+      </Paragraph>
+      <Paragraph>
         Sony were impressed, so they funded SP&#39;s Sly Cooper trilogy for the
         PS2 and inFamous 1/2 on the PS3. After five great games under this
         partnership, Sony acquired the studio in 2011.
-      </p>
-      <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
+      </Paragraph>
+      <Paragraph>
         SP have since moved on from inFamous to Ghost of Tsushima. A
         third-person samurai action-adventure that quickly became the
         studio&#39;s best selling game.
-      </p>
-      <p className="mb-8 text-lg lg:text-xl lg:tracking-wide">
+      </Paragraph>
+      <Paragraph marginBottom={8}>
         GoT initially released on PS4 in 2020, followed by a PS5 Director&#39;s
         Cut in 2021 that added around 10 more hours of gameplay via a new island
         to explore. It&#39;s currently SP&#39;s only PS5 game.
-      </p>
-      <div className="md:flex items-center">
+      </Paragraph>
+      <ImageSection flexOnMdScreens={true}>
         <div>
-          <h2 className="mb-3 text-gray-800 text-2xl font-semibold pb-1">
-            <span className="border-b-2 border-gray-300">
-              My Analysis & Predictions
-            </span>
-          </h2>
-          <p className="mb-5 text-lg lg:text-xl lg:tracking-wide">
+          <DeveloperSubTitle subtitle="My Analysis & Predictions" />
+          <Paragraph>
             It&#39;s been nearly four years since SP achieved new highs, both
             commercially and critically, with GoT. It&#39;s therefore not
             surprising that a proper sequel is heavily rumoured to be their next
             game.
-          </p>
-          <p className="mb-8 text-lg lg:text-xl lg:tracking-wide">
+          </Paragraph>
+          <Paragraph marginBottom={8}>
             Considering this, I predict that not only is GoT 2 the next game ANY
             PS developer announces, but it will also release very shortly after
             to new highs for Sucker Punch, critically and commercially.
-          </p>
+          </Paragraph>
         </div>
         <div>
           <Image
@@ -81,37 +70,10 @@ function SuckerPunchProductionsPage(): JSX.Element {
             GoT: Director&#39;s Cut Main Menu Screen
           </label>
         </div>
-      </div>
-      <h2 className="mb-5 text-gray-800 text-2xl font-semibold pb-1">
-        <span className="border-b-2 border-gray-300">PS5 Games</span>
-      </h2>
-      <div className="mb-5 grid gap-5 sm:grid-cols-2">
-        {games
-          .filter((game: Game) => game.developer === "Sucker Punch Productions")
-          .map((game: Game) => (
-            <div
-              key={game.id}
-              className="border-2 border-gray-200 rounded-lg shadow-md p-3"
-            >
-              <h3 className="text-xl font-bold text-center mb-1 lg:text-2xl lg:mb-3">
-                {game.name}
-              </h3>
-              <p className="text-lg lg:text-xl lg:tracking-wide">
-                Genre: {game.genre}
-              </p>
-              <p className="text-lg lg:text-xl lg:tracking-wide">
-                Release Date: {game.releaseDate}
-              </p>
-              <p className="text-lg lg:text-xl lg:tracking-wide">
-                Days Since Their Last Game: {game.daysBetweenReleases}
-              </p>
-              <p className="text-lg lg:text-xl lg:tracking-wide">
-                Metacritic Score: {game.metacritic}
-              </p>
-            </div>
-          ))}
-      </div>
-    </div>
+      </ImageSection>
+      <DeveloperSubTitle subtitle="PS5 Games" marginBottom={5} />
+      <GamesSection games={gamesArray} developer="Sucker Punch Productions" />
+    </Container>
   );
 }
 

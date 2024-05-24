b79c131d19a3017412dd62a5415ec63bf09a06a4
acktreptow <acktreptow@gmail.com>
2024-05-24 04:32:15 +0100

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,14 @@ export const metadata: Metadata = {
     icon: "images/favicon.ico",
   },
   openGraph: {
-    title: "PS5 Studios - The site about PlayStation Studios for PS5.",
+    title: "PS5 Studios - The Site About PlayStation Studios For PS5.",
     description:
       "Analyzing the first-party developers under PlayStation Studios during the PS5 era.",
     url: "https://ps5studios.com",
     siteName: "PS5 Studios",
     images: [
       {
-        url: "images/opengraph-image.png",
+        url: "../../public/images/opengraph-image.png",
         width: 1200,
         height: 630,
         alt: "PS5 Studios",

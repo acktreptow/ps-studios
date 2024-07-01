7515e41ff1aada18b6856cfd5141535946c9a85e
acktreptow <acktreptow@gmail.com>
2024-07-01 18:37:28 +0100

    update paragraph children

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,11 @@ function NotFound() {
   return (
     <Container>
       <Title title="404 Not Found" />
-      <Paragraph>Could not find requested resource</Paragraph>
+
+      <Paragraph>
+        This page doesn&#39;t exist. Please navigate to another page by clicking
+        a link in the header or using the button below.
+      </Paragraph>
       <button
         type="submit"
         className="block mx-auto border p-4 mb-8 bg-playstation text-white rounded-full hover:bg-blue-900 transition-colors duration-200 shadow-md shadow-playstation"

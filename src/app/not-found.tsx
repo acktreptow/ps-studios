2fe565fec301b5d3f37b49308aacac0b5746f7a7
acktreptow <acktreptow@gmail.com>
2024-07-01 18:43:03 +0100

    create div

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,11 +7,12 @@ function NotFound() {
   return (
     <Container>
       <Title title="404 Not Found" />
-
-      <Paragraph>
-        This page doesn&#39;t exist. Please navigate to another page by clicking
-        a link in the header or using the button below.
-      </Paragraph>
+      <div className="bg-playstation rounded-lg md:mx-auto md:w-3/4">
+        <p className="text-lg text-center text-white mb-10 p-4 lg:text-xl lg:tracking-wide">
+          This page doesn&#39;t exist. Please go to another page by clicking a
+          link in the header or the button below.
+        </p>
+      </div>
       <button
         type="submit"
         className="block mx-auto border p-4 mb-8 bg-playstation text-white rounded-full hover:bg-blue-900 transition-colors duration-200 shadow-md shadow-playstation"

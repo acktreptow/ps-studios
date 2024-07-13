b6dad2d4ffe976b36f338d1f41c99d2551ede653
acktreptow <acktreptow@gmail.com>
2024-07-13 23:02:07 +0100

    create colored div

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -17,21 +17,23 @@ function DevelopersPage(): JSX.Element {
       <h1 className="text-3xl font-bold py-2 mb-3 md:text-5xl md:mb-10">
         PlayStation Developers
       </h1>
-      <ul className="text-xl leading-8 md:text-2xl md:leading-10">
-        {developers
-          .filter(
-            (developer: Developer) =>
-              developer.name !== "PlayStation Logo" &&
-              developer.name !== "PS5 Logo"
-          )
-          .map((developer: Developer) => (
-            <Link key={developer.id} href={`developers/${developer.urlPath}`}>
-              <li className="font-semibold p-1 hover:text-playstation transition duration-200">
-                {developer.name}
-              </li>
-            </Link>
-          ))}
-      </ul>
+      <div className="border bg-playstation rounded-3xl xl w-1/3 mx-auto p-4">
+        <ul className="text-white text-xl leading-8 md:text-2xl">
+          {developers
+            .filter(
+              (developer: Developer) =>
+                developer.name !== "PlayStation Logo" &&
+                developer.name !== "PS5 Logo"
+            )
+            .map((developer: Developer) => (
+              <Link key={developer.id} href={`developers/${developer.urlPath}`}>
+                <li className="font-semibold p-1 hover:text-playstation transition duration-200">
+                  {developer.name}
+                </li>
+              </Link>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }

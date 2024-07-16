a64c2e53619ced6a9fe55c1403ca735b5069abd2
acktreptow <acktreptow@gmail.com>
2024-07-16 04:12:30 +0100

    update bg color gradient

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer(): JSX.Element {
   return (
-    <footer className=" bg-playstation text-white text-center py-4 border-t-8 border-amber-300">
+    <footer className=" bg-gradient-to-b from-blue-700 to-playstation text-white text-center py-4 border-t-8 border-amber-300">
       <p className="font-semibold lg:text-lg">
         PS5 Studios All Rights Reserved © 2024
       </p>

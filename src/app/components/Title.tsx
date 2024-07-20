f67e786fc90321abe2e00e2c57663a82ad452e13
acktreptow <acktreptow@gmail.com>
2024-07-20 23:30:07 +0100

    update lg breakpoint

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -4,7 +4,7 @@ type DeveloperTitleProps = {
 
 function DeveloperTitle({ title }: DeveloperTitleProps): JSX.Element {
   return (
-    <h1 className="text-4xl text-center font-bold mb-10 text-gray-950 tracking-wide md:leading-relaxed">
+    <h1 className="text-4xl text-center font-bold mb-10 text-gray-950 tracking-wide md:leading-relaxed lg:text-6xl">
       {title}
     </h1>
   );

65116c61064ea24c2d236fef3e52781aea6525cf
Alex Treptow <acktreptow@gmail.com>
2024-12-06 15:27:07 -0800

    create studio closures page

---
+++ src/app/studioclosures/page.tsx
@@ -0,0 +1,10 @@
+function studioClosuresPage() {
+  return (
+    <div>
+      <h1>Studio Closures</h1>
+      <p>Studio closures are listed here.</p>
+    </div>
+  );
+}   
+
+export default studioClosuresPage;

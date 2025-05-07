f738f87d509d54a75bb053490ae8c97a7c00f376
acktreptow <acktreptow@gmail.com>
2025-05-07 04:48:56 +0100

    rewrite asobi analysis and predictions section

--- src/app/developers/teamasobi/page.tsx
+++ src/app/developers/teamasobi/page.tsx
@@ -46,36 +46,34 @@ function TeamAsobiPage(): JSX.Element {
         closure.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        Asobi&#39;s next game, Astro Bot, came out very recently in September 2024.
-        Its nearly 4-year development cycle resulted in it being Asobi&#39;s biggest
-        game to date. But most importantly, it led to an excellent 94 on
-        Metacritic, making Astro Bot the highest rated game of 2024 and tying
-        with God of War: Ragnarök as the highest rated first-party PS5 game.
+        Asobi&#39;s next game, Astro Bot, came out very recently in September
+        2024. Its nearly 4-year development cycle resulted in it being
+        Asobi&#39;s biggest game to date. But most importantly, it led to an
+        excellent 94 on Metacritic, making Astro Bot the highest rated game of
+        2024 and tying with God of War: Ragnarök as the highest rated
+        first-party PS5 game.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            Despite Astro&#39;s Playroom releasing in 2020, it took until May
-            2024 for Team Asobi to announce their next game, simply called Astro
-            Bot - their biggest game yet which released in September.
-          </Paragraph>
-          <Paragraph>
-            Astro&#39;s Playroom was a great game that even after 4 years, has
-            still made the best use of the DualSense. It&#39;s why Asobi
-            survived the Japan Studio closure.
-          </Paragraph>
-          <Paragraph>
-            It&#39;s therefore no surprise that this experience, combined with
-            Astro Bot having double the development time, resulted in a 94 on
-            Metacritic. Making Astro Bot the highest rated game of 2024 and
-            tieing with God of War: Ragnarok as the highest rated first-party
-            PS5 game.
+            Astro Bot&#39;s monumental success must have come as a surprise to
+            Sony. Outside of Metacritic Astro Bot also topped OpenCritic in 2024
+            at 99%. Astro Bot is also officially the most awarded platform of
+            all time with 195 Game of the Year nominations and wins, including
+            winning Game of the Year at the 2024 Game Awards. The game has also
+            sold close to 2.5 million copies and is now part of numerous PS5
+            bundles. Considering its poultry development budget of around $50
+            million, the game has also returned a significant profit.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            It&#39;s still too early for sales figures, but it&#39;s been at the
-            top of sales charts worldwide. A desperately needed win for Sony
-            after Concord.
+            Considering the unprecedented critical success, and still very good
+            commercial success, the world is truly Asboi&#39;s oyster. Whatever
+            they want to do next, I can&#39;t imagine Sony says no. Astro Bot
+            was the blueprint for them to be successful in the current games
+            market, so I just hope they stick to the gameplan and we get an
+            excellent follow-up close to the end of the PS5&#39;s lifecycle in
+            2027/28.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

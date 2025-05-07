a7c1a512fb24bf9493e175c52bf26c38f436ceb8
acktreptow <acktreptow@gmail.com>
2025-05-07 03:19:22 +0100

    edit down nk analysis and predictions section

--- src/app/developers/neonkoi/page.tsx
+++ src/app/developers/neonkoi/page.tsx
@@ -26,38 +26,36 @@ function NeonKoiPage(): JSX.Element {
       <Paragraph>
         Neon Koi was a German-Finnish developer that was founded in 2020 under
         the name &#39;Savage Game Studios&#39;. It was under this initial name
-        when Sony acquired them in August, 2022.
+        when Sony acquired them in August 2022.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        When it comes to Neon Koi&#39;s PS5 output, they haven&#39;t released
-        anything. This is because they were a founding member of PlayStation
-        Studios&#39;s Mobile Division and purchased to exclusively develop
-        mobile games.
+        When it comes to Neon Koi&#39;s PS5 output, they didn&#39;t release
+        anything. That&#39;s because they were a founding member of PlayStation
+        Studios&#39;s Mobile Division and acquired to exclusively develop mobile
+        games.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
             Still interested in their mobile offerings? Well, don&#39;t hold
-            your breath as Neon Koi was shut down by PlayStation in October,
-            2024. Despite forming over four years before closure, Neon Koi
-            didn&#39;t even announce a game, let alone show gameplay or actually
-            release something.
+            your breath as Neon Koi was shut down in October 2024. And despite
+            forming over four years before closure, Neon Koi didn&#39;t even
+            announce a game, let alone show gameplay or actually ship something.
           </Paragraph>
           <Paragraph>
-            In fact, Neon Koi was only in the news for negative headlines. Like
-            when the head of PlayStation&#39;s Mobile Division and an SGS
-            founder abruptly left under suspicious circumstances, or the failed
+            In fact, Neon Koi was only known for negative headlines. Like when
+            the head of PlayStation&#39;s Mobile Division and an SGS founder
+            abruptly quit under suspicious circumstances, or the failed
             rebranding to Neon Koi in 2023.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            In the end, at best, Neon Koi will be remembered as an insignificant
-            footnote in PlayStation&#39;s history during the PS5 era — and at
-            worst, as a glaring example of the mistakes made by former
-            PlayStation leadership. From short-sighted, poorly researched
-            acquisitions to wasted spending on games that were cancelled before
-            they were even announced, the studio embodies the missteps of the
-            Jim Ryan era.
+            At best, Neon Koi will be remembered as an insignificant footnote in
+            PlayStation&#39;s history during the PS5 era — and at worst, as a
+            glaring example of the mistakes made by former PlayStation
+            leadership. From short-sighted, poorly researched acquisitions to
+            wasted spending on games that were cancelled before they were even
+            announced, NK embodies the missteps of the Jim Ryan era.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

1144de311b607409ea2c79125fa850abb3b10958
acktreptow <acktreptow@gmail.com>
2025-05-09 19:02:17 +0100

    rewrite san diego studio analysis and predictions section

--- src/app/developers/sandiegostudio/page.tsx
+++ src/app/developers/sandiegostudio/page.tsx
@@ -51,20 +51,26 @@ function SanDiegoStudioPage(): JSX.Element {
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            This one is easy to predict. SD Studio haven&#39;t developed a
-            non-MLB game for close to a decade. And with rising development
-            costs coupled with tighter profits for PlayStation, it&#39;s
-            unlikely they would be allowed to experiment with something new,
-            even if they want to.
+            This one is easy to predict. SD Studio hasn&#39;t been the primary
+            developer on a non-MLB game since 2016&#39;s Kill Strain.
+            Considering rising development costs coupled with external economic
+            factors like tariffs and inflation, it&#39;s also unlikely they
+            would be allowed to experiment with something new, even if they want
+            to.
           </Paragraph>
           <Paragraph>
-            We&#39;re also four games deep into the series being multiplatform
-            and nothing has materially changed. That was the one change that
-            could have altered SD Studio&#39;s trajectory, but didn&#39;t.
+            We&#39;re also 5 games deep into the series becoming multiplatform
+            and nothing has materially changed. MLB going into Xbox Gamepass was
+            the one change that could have altered SD Studio&#39;s trajectory,
+            but even that didn&#39;t, with Xbox obviously not seeing the
+            benefits as they didn&#39;t renew their original contract to have it
+            appear day 1 in their subscription service.
           </Paragraph>
           <Paragraph marginBottom={8}>
             It&#39;s therefore safe to assume they&#39;ll continue with MLB: The
-            Show for the foreseeable future.
+            Show for the foreseeable future. However, MLB 25 was the first to
+            skip the PS4/Xbox One, so the only big change is the move from
+            Switch 1 to Switch 2 for MLB The Show 26.
           </Paragraph>
         </div>
         <div>

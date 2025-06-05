57e40c82084ded3843dd5c73b4d51af5d2d0ed99
acktreptow <acktreptow@gmail.com>
2025-06-06 00:52:33 +0100

    create visual page with placeholder text

---
+++ src/app/developers/visualarts/page.tsx
@@ -0,0 +1,74 @@
+import { Metadata } from "next";
+import gamesData from "../../data/games.json";
+import { Game } from "../../types/Game";
+import Image from "next/image";
+import gummyBears from "../../../../public/images/dev-pages/gummy-bears.png";
+import Container from "../../components/Container";
+import Title from "../../components/Title";
+import DeveloperButton from "../../components/DeveloperButton";
+import SubTitle from "../../components/SubTitle";
+import Paragraph from "../../components/Paragraph";
+import ImageSection from "../../components/ImageSection";
+
+export const metadata: Metadata = {
+  title: "Visual Arts",
+  description:
+    "The history and future of Visual Arts, a first-party developer for PlayStation Studios.",
+};
+
+const gamesArray: Game[] = gamesData;
+
+function VisualArtsPage(): JSX.Element {
+  return (
+    <Container>
+      <Title title="Visual Arts" />
+      <DeveloperButton
+        developer="teamLFG"
+        urlPath="https://www.playstation.com/corporate/playstation-studios/teamlfg/"
+      />
+      <SubTitle subtitle="History" />
+      <Paragraph>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+        expedita facere libero voluptates veniam recusandae dolor, harum ad?
+        Quidem eaque unde distinctio nam eos necessitatibus laborum inventore
+        ducimus. Amet, ex!
+      </Paragraph>
+      <Paragraph marginBottom={8}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+        expedita facere libero voluptates veniam recusandae dolor, harum ad?
+        Quidem eaque unde distinctio nam eos necessitatibus laborum inventore
+        ducimus. Amet, ex!
+      </Paragraph>
+      <ImageSection flexOnMdScreens={false}>
+        <div>
+          <SubTitle subtitle="My Analysis & Predictions" />
+          <Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
+          <Paragraph marginBottom={8}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
+        </div>
+        <div className="md:flex md:flex-col md:items-center">
+          <Image
+            src={gummyBears}
+            alt="Gummy Bears concept art"
+            id="gummy-bears"
+            className="my-3 rounded-lg md:ml-5 md:max-w-xs lg:max-w-md"
+          />
+          <label htmlFor="gummy-bears" className="block text-center mb-8">
+            The Only Concept Art Of Project Gummy Bears Currently
+          </label>
+        </div>
+      </ImageSection>
+    </Container>
+  );
+}
+
+export default VisualArtsPage;

15013c8e41d60900ada82444f2591b0b179b959e
acktreptow <acktreptow@gmail.com>
2025-06-05 06:37:17 +0100

    create manchester studio page

---
+++ src/app/developers/manchesterstudio/page.tsx
@@ -0,0 +1,67 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import concreteGenie from "../../../../public/images/dev-pages/concrete-genie.png";
+import Container from "../../components/Container";
+import Title from "../../components/Title";
+import DeveloperButton from "../../components/DeveloperButton";
+import SubTitle from "../../components/SubTitle";
+import Paragraph from "../../components/Paragraph";
+import ImageSection from "../../components/ImageSection";
+
+export const metadata: Metadata = {
+  title: "Manchester Studio",
+  description:
+    "The history of Manchester Studio, a former first-party developer for PlayStation Studios.",
+};
+
+function SanMateoStudioPage(): JSX.Element {
+  return (
+    <Container>
+      <Title title="Manchester Studio" />
+      <DeveloperButton
+        developer="Pixelopus"
+        urlPath="https://x.com/pixelopus?lang=en"
+      />
+      <SubTitle subtitle="History" />
+      <Paragraph>
+        Manchester was founded internally in Sony Interactive in 2015. The UK
+        developer was created with the sole purpose of developing virtual
+        reality titles for the upcoming PSVR.
+      </Paragraph>
+      <Paragraph marginBottom={8}>
+        Manchester Studio were shut down in 2020, before they even released a
+        game.
+      </Paragraph>
+      <ImageSection flexOnMdScreens={true}>
+        <div>
+          <SubTitle subtitle="My Analysis & Predictions" />
+          <Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
+          <Paragraph marginBottom={8}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
+        </div>
+        <div>
+          <Image
+            src={concreteGenie}
+            alt="Concrete Genie"
+            id="concrete-genie"
+            className="my-3 rounded-lg md:ml-5 md:max-w-xs lg:max-w-md"
+          />
+          <label htmlFor="concrete-genie" className="block text-center">
+            Pixelopus&#39;s Final Game: Concrete Genie
+          </label>
+        </div>
+      </ImageSection>
+    </Container>
+  );
+}
+
+export default SanMateoStudioPage;

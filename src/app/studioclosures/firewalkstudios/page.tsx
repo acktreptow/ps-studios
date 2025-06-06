25a5f6bdbdab7afdd32f514d1b1c6ae07315efcf
acktreptow <acktreptow@gmail.com>
2025-06-06 19:10:43 +0100

    remove concord key art image

--- src/app/studioclosures/firewalkstudios/page.tsx
+++ src/app/studioclosures/firewalkstudios/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import gamesData from "../../data/games.json";
 import { Game } from "../../types/Game";
 import Image from "next/image";
-import concord from "../../../../public/images/dev-pages/concord.png";
 import Container from "../../components/Container";
 import Title from "../../components/Title";
 import DeveloperButton from "../../components/DeveloperButton";
@@ -30,55 +29,49 @@ function FirewalkStudiosPage(): JSX.Element {
       <SubTitle subtitle="History" />
       <Paragraph>
         This Washington-based developer was founded in 2018 by senior veterans
-        of popular shooters like Halo and Destiny. With this prestige multiplayer expertise and Sony&#39;s 
-        increased focus on live-service games for PS5 & PC, it&#39;s therefore no surprise they
-        acquired Firewalk in early 2023.
+        of popular shooters like Halo and Destiny. With this prestige
+        multiplayer expertise and Sony&#39;s increased focus on live-service
+        games for PS5 & PC, it&#39;s therefore no surprise they acquired
+        Firewalk in early 2023.
       </Paragraph>
       <Paragraph marginBottom={8}>
         Firewalk then announced their first game, Concord, shortly after.
-        It&#39;s a sci-fi 5vs5 live-service shooter that
-        released on PS5 and PC in August 2024.
+        It&#39;s a sci-fi 5vs5 live-service shooter that released on PS5 and PC
+        in August 2024.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            It wasn&#39;t until May 2024 before we actually saw Concord, via trailers that had the story and gameplay
-           resemble a Temu Guardians of the Galaxy and Overwatch respectively.
+            It wasn&#39;t until May 2024 before we actually saw Concord, via
+            trailers that had the story and gameplay resemble a Temu Guardians
+            of the Galaxy and Overwatch respectively.
           </Paragraph>
           <Paragraph>
-            The playable betas in July didn&#39;t help either. Character designs were universally negative 
-            and the gameplay wasn&#39;t good enough to justify $40.
-            Especially compared to its F2P competitors like Fortnite.
+            The playable betas in July didn&#39;t help either. Character designs
+            were universally negative and the gameplay wasn&#39;t good enough to
+            justify $40. Especially compared to its F2P competitors like
+            Fortnite.
           </Paragraph>
           <Paragraph>
-            This all led to a historically poor launch for Sony. It&#39;s tied with
-            Firewall Ultra as the worst reviewed PS Studios PS5 game on
-            Metacritic at 62 and only sold 25k copies. Totalling around $1 million in revenue
-            despite a $200 million+ budget.
+            This all led to a historically poor launch for Sony. It&#39;s tied
+            with Firewall Ultra as the worst reviewed PS Studios PS5 game on
+            Metacritic at 62 and only sold 25k copies. Totalling around $1
+            million in revenue despite a $200 million+ budget.
           </Paragraph>
           <Paragraph>
-            It was so bad that Sony removed Concord from sale 
-            and refunded all purchases just 11 days later. Firewalk explored options on how to bring back Concord in a way
-            that resonates, but on October 29th, Firewalk was shut down along with
-            confirmation that Concord won&#39;t return. 
+            It was so bad that Sony removed Concord from sale and refunded all
+            purchases just 11 days later. Firewalk explored options on how to
+            bring back Concord in a way that resonates, but on October 29th,
+            Firewalk was shut down along with confirmation that Concord
+            won&#39;t return.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            This was the first time Sony refunded a game due to its
-            reception. It also resulted in over $200 million in losses and led to Firewalk closing. Concord and Firewalk are therefore 
-            easily the biggest PS Studios disappointment of the PS5 era.
+            This was the first time Sony refunded a game due to its reception.
+            It also resulted in over $200 million in losses and led to Firewalk
+            closing. Concord and Firewalk are therefore easily the biggest PS
+            Studios disappointment of the PS5 era.
           </Paragraph>
-        </div>
-        <div className="md:flex md:flex-col md:items-center">
-          <Image
-            src={concord}
-            alt="Concord key art"
-            id="concord"
-            className="my-3 rounded-lg md:ml-5 md:max-w-xs lg:max-w-md"
-          />
-          <label htmlFor="concord" className="block text-center mb-8">
-            Concord Primary Key Art
-          </label>
         </div>
       </ImageSection>
       <SubTitle subtitle="PS5 Games" marginBottom={5} />

73380309535f2c015aab791819722943d3dc3588
acktreptow <acktreptow@gmail.com>
2025-06-14 04:23:43 +0100

    write baseline draft for visual arts analysis and predictions section

--- src/app/developers/visualarts/page.tsx
+++ src/app/developers/visualarts/page.tsx
@@ -43,16 +43,29 @@ function VisualArtsPage(): JSX.Element {
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-            expedita facere libero voluptates veniam recusandae dolor, harum ad?
-            Quidem eaque unde distinctio nam eos necessitatibus laborum
-            inventore ducimus. Amet, ex!
+            While Visual Arts will have a small role in pretty much every PS
+            Studios game that comes out during the PS5 generation, it&#39;s now
+            very unlikely that they will get to make their own game before the
+            generation finishes.
+          </Paragraph>
+          <Paragraph>
+            Higher ups like Mumbauer are now gone, and due to the worse economy
+            since originally working on TLOU1 as the main developer, they have
+            been affected by the many rounds of Sony layoffs. Including in March
+            2025 where it was reported that some VA staff who were working on
+            Bend&#39;s cancelled live-service game had been let go due to the
+            cancellation.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-            expedita facere libero voluptates veniam recusandae dolor, harum ad?
-            Quidem eaque unde distinctio nam eos necessitatibus laborum
-            inventore ducimus. Amet, ex!
+            Perhaps circumstances change during the PS6 generation and Visual
+            Arts are able to finally do what they set out to back in 2018. Until
+            then, if this scenario ever comes about, we can at least appreciate
+            their work in pretty much every future PS Studios game through their
+            producing art, animation and technical support. It was also
+            announced last year that Visual Arts, alongside a new internal Sony
+            studio, would be assisting Naughty Dog on a title based on a
+            &#39;beloved franchise,&#39; though still no word on what that game
+            is.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

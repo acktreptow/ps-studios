2e2810a2d97281ae72ccc7bcb78c1a1c691c00e6
acktreptow <acktreptow@gmail.com>
2025-06-14 03:53:47 +0100

    write out baseline for london studio history section

--- src/app/developers/londonstudio/page.tsx
+++ src/app/developers/londonstudio/page.tsx
@@ -24,20 +24,49 @@ function LondonStudioPage(): JSX.Element {
       />
       <SubTitle subtitle="History" />
       <Paragraph>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        expedita facere libero voluptates veniam recusandae dolor, harum ad?
-        Quidem eaque unde distinctio nam eos necessitatibus laborum inventore
-        ducimus. Amet, ex!
+        London Studio was founded in 2002, following their merger of SCE Studio
+        Camden (formerly a division of then-first party developer Psygnosis) and
+        Team Soho. While they did have some traditional IP such as the
+        open-world Getaway series and also helped assist with the development of
+        traditional games at other studios like Killzone 2 and LittleBigPlanet,
+        London Studio has always been one of Sony&#39;s most experimental teams
+        as they primarily worked on games that used the myriad of peripherals
+        PlayStation created for its various consoles.
+      </Paragraph>
+      <Paragraph>
+        By far, the most successful series London Studio developed was the
+        Singstar IP that managed to sell more than 20 million copies in 6 years
+        and spanned numerous games from PS2-PS4. However their other IP such as
+        the PS2&#39;s EyeToy, Wonderbook and PlayStation Home for PS3 and
+        PSVR&#39;s Blood and Truth were also commercially and/or successful.
+      </Paragraph>
+      <Paragraph>
+        London Studio&#39;s final game was 2019&#39;s Erica. An interactive film
+        game that took advantage of PS4&#39;s PlayLink, which allowed the game
+        to be interfaced with a smartphone. The game received a mixed response,
+        which combined with PSVR&#39;s failing popularity and PS Studios pivot
+        to live-service, is no surprise that&#39;s the London Studio also went.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        expedita facere libero voluptates veniam recusandae dolor, harum ad?
-        Quidem eaque unde distinctio nam eos necessitatibus laborum inventore
-        ducimus. Amet, ex!
+        Of course, this was also not meant to be as the game was cancelled along
+        with the closure of London Studio in early 2024 as part of wider
+        layoffs.
       </Paragraph>
       <ImageSection flexOnMdScreens={true}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
+          <Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            expedita facere libero voluptates veniam recusandae dolor, harum ad?
+            Quidem eaque unde distinctio nam eos necessitatibus laborum
+            inventore ducimus. Amet, ex!
+          </Paragraph>
           <Paragraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
             expedita facere libero voluptates veniam recusandae dolor, harum ad?

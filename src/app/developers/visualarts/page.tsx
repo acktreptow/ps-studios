a88e2796cf7e75a9320b89ca8fb60644e0f69a1e
acktreptow <acktreptow@gmail.com>
2025-06-16 08:00:27 +0100

    rewrite baseline visual arts analysis and predictions section to version 1

--- src/app/developers/visualarts/page.tsx
+++ src/app/developers/visualarts/page.tsx
@@ -99,29 +99,34 @@ function VisualArtsPage(): JSX.Element {
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            While Visual Arts will have a small role in pretty much every PS
-            Studios game that comes out during the PS5 generation, it&#39;s now
-            very unlikely that they will get to make their own game before the
+            Visual Arts will continue to have a small role in pretty much every
+            PS Studios game that comes out during the PS5 generation. But
+            considering what happened to Mumbauer, it&#39;s now also very
+            unlikely that they will get to make their own PS5 game before the
             generation finishes.
           </Paragraph>
           <Paragraph>
-            Higher ups like Mumbauer are now gone, and due to the worse economy
-            since originally working on TLOU1 as the main developer, they have
-            been affected by the many rounds of Sony layoffs. Including in March
-            2025 where it was reported that some VA staff who were working on
-            Bend&#39;s cancelled live-service game had been let go due to the
-            cancellation.
+            Furthermore, since Mumbauer and other higher ups left VASG after the
+            failed attempt, the economy has only gotten worse with companies all
+            over the world doing multiple rounds of layoffs to combat this, and
+            Sony is no different. In March 2025, numerous VASG staff supporting
+            Bend&#39;s now cancelled live-service were let go. As for the rest
+            of the staff, there&#39;s already rumors that they&#39;re once again
+            assisting Naughty Dog, alongside a new internal Sony studio, on a
+            title based on a &#39;beloved franchise&#39;, which may well be a
+            new Uncharted. Although these rumors are from 2022, and we&#39;ve
+            seen from numerous other developers such as Bend. Bluepoint,
+            Firewalk, Bungie and even Naughty Dog how much can change in 3
+            years.
           </Paragraph>
           <Paragraph marginBottom={8}>
             Perhaps circumstances change during the PS6 generation and Visual
-            Arts are able to finally do what they set out to back in 2018. Until
-            then, if this scenario ever comes about, we can at least appreciate
-            their work in pretty much every future PS Studios game through their
-            producing art, animation and technical support. It was also
-            announced last year that Visual Arts, alongside a new internal Sony
-            studio, would be assisting Naughty Dog on a title based on a
-            &#39;beloved franchise,&#39; though still no word on what that game
-            is.
+            Arts are able to finally do what they set out to back in 2018 and
+            create their own remake, which they’ll use as a springboard to
+            eventually make their own PS6 game. Until then, if this unlikely
+            scenario ever comes about, we can at least appreciate their work in
+            pretty much every future PS Studios game through them providing art,
+            animation and technical support.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

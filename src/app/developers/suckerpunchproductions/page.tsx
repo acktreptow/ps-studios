58d28a634da32c1a67bc79a3caca237d4c76c338
acktreptow <acktreptow@gmail.com>
2025-06-26 19:34:18 +0100

    update sucker punch sections

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -30,19 +30,15 @@ function SuckerPunchProductionsPage(): JSX.Element {
       <SubTitle subtitle="History" />
       <Paragraph>
         Sucker Punch are a Washington-based developer that was founded in 1997,
-        initially to work on an N64 game called Rocket: Robot on Wheels.
-      </Paragraph>
-      <Paragraph>
-        Sony were impressed, so they funded SP&#39;s Sly Cooper trilogy for the
-        PS2 and inFamous 1/2 on the PS3. After five great games under this
+        initially to work on an N64 game called Rocket: Robot on Wheels. Sony
+        were impressed, so they funded SP&#39;s Sly Cooper trilogy for the PS2
+        and inFamous 1/2 on the PS3. After five great games under this
         partnership, Sony acquired the studio in 2011.
       </Paragraph>
-      <Paragraph>
+      <Paragraph marginBottom={8}>
         SP have since moved on from inFamous to 2020&#39;s Ghost of Tsushima.
         This third-person samurai action-adventure is the PS4&#39;s
         fastest-selling first-party debut at 2.4 million copies sold in 3 days.
-      </Paragraph>
-      <Paragraph marginBottom={8}>
         Tsushima then got a PS5 Director&#39;s Cut in 2021, which is currently
         SP&#39;s only released PS5 game. It added around 10 more hours of
         gameplay via a new island to explore and was even better critically
@@ -55,17 +51,16 @@ function SuckerPunchProductionsPage(): JSX.Element {
           <Paragraph>
             Tsushima is Sucker Punch&#39;s best selling game, so it&#39;s not
             surprising that they&#39;ve stuck with the IP for Ghost of Yōtei - a
-            standalone sequel that takes place 329 years after Tsushima. Despite
-            being revealed in September 2024, Yōtei is already coming out on
-            October 2, 2025.
+            standalone sequel taking place 329 years later. Despite its recent
+            September 2024 reveal, Yōtei is already coming out in October 2025.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            As this is SP&#39;s second time with the IP, I predict Yōtei
-            surpasses Tsushima critically because they can now correct any
-            mistakes made the first time. However, when comparing the length of
-            game development with how far we&#39;re into the PS5 lifecycle, I
-            imagine Yōtei is SP&#39;s final PS5 game. Though the larger PS5
-            install base means Yōtei should surpass Tsushima commercially too.
+            As it&#39;s SP&#39;s second time with the IP, I predict Yōtei
+            surpasses Tsushima critically because they can now refine the
+            already successful formula. The larger PS5 install base means Yōtei
+            will surpass the Tsushima DC commercially too. Finally, due to the
+            length of game development with how far we&#39;re into the PS5
+            lifecycle, I also imagine Yōtei is SP&#39;s final PS5 game.
           </Paragraph>
         </div>
         <div>

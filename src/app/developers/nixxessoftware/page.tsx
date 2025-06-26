12b37bc482331fc1fd7c623bfc1f8e69e89c3943
acktreptow <acktreptow@gmail.com>
2025-06-26 06:21:33 +0100

    edit down nixxes analysis and predictions section

--- src/app/developers/nixxessoftware/page.tsx
+++ src/app/developers/nixxessoftware/page.tsx
@@ -32,37 +32,36 @@ function NixxesSoftwarePage(): JSX.Element {
         Nixxes primarily develop the PC ports of them.
       </Paragraph>
       <Paragraph>
-        Since 2022, Nixxes has already shipped 8 fully-featured (DLSS etc) PC
-        ports. As they aren&#39;t new games that need a big marketing cycle, the
-        time from announcement to release is very short. It&#39;s therefore hard
-        to predict their next port.
+        Since 2022, Nixxes has already shipped 8 fully-featured PC ports. As
+        they aren&#39;t new games that need a big marketing cycle, the time from
+        announcement to release is very short. It&#39;s therefore hard to
+        predict the next.
       </Paragraph>
       <Paragraph marginBottom={8}>
         However, there&#39;s one port that stands out for PS5 fans - 2017&#39;s
-        Horizon Zero Dawn remaster. Since acquisition, this is is the first port
-        where Nixxes simultaneously shipped a PS5 version and is certainly a
-        sign of things to come.
+        Horizon Zero Dawn remaster. Since acquisition, this 2025 title is the
+        first time Nixxes simultaneously shipped both a PS5 and PC version, and
+        is certainly a sign of things to come.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            Nixxes are a very important studio for PlayStation as PC is the
-            easiest way Sony finds growth outside of the console market. A
-            necessity as AAA development costs continue to rise.
+            Nixxes are very important for PlayStation as PC is the easiest way
+            Sony finds growth outside of the console market - a necessity as AAA
+            development costs continue to rise. However, with PS5 games that
+            need a PC port drying up, Nixxes are in a unique position to go back
+            into the PS3/PS4 catalogue and reintroduce classics to a new
+            audience.
           </Paragraph>
-          <Paragraph>
-            However, with PS5 games that need a PC port drying up, Nixxes are in
-            a unique position to go back into the PS3/PS4 catalogue and
-            reintroduce classics to a new audience.
-          </Paragraph>
+
           <Paragraph>
             I predict this is more likely than Nixxes simultaneously releasing
             PC ports of new PS5 games. Logistically, this is a nightmare for an
-            external studio to do so effectively unless the main PS Studio holds
-            their finished PS5 version until the PC port is ready. It&#39;s also
-            unclear if Sony even believe day 1 PC ports is what&#39;s best for
-            their singleplayer games, or if it results in fewer PS5s sold.
+            external studio to do so effectively unless the finished PS5 version
+            is held until the PC port is ready. It&#39;s also unclear if Sony
+            even believe day 1 PC ports is what&#39;s best for their
+            singleplayer games.
           </Paragraph>
           <Paragraph marginBottom={8}>
             Regardless, it&#39;s great to see Nixxes finally ship a PS5 game.
@@ -70,9 +69,8 @@ function NixxesSoftwarePage(): JSX.Element {
             original launching only 8 years ago, it bodes well for other games
             around this era that Nixxes could port. Especially older PS4 titles
             that released before the PS4 Pro, like Killzone: Shadow Fall. That
-            along with the PS2/3 Killzone trilogy would be a best case scenario
-            as Nixxes wait for more first-party PS5 games to come out and port
-            to PC.
+            would be a best case scenario as Nixxes wait for more first-party
+            PS5 games to come out and port to PC.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

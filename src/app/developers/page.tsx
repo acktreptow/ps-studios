78b17aed10c5cf98e3e790b9d1f88850009f886e
acktreptow <acktreptow@gmail.com>
2025-06-27 21:46:48 +0100

    update developers list responsive page

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -14,34 +14,42 @@ const developers: Developer[] = developersData;
 function DevelopersPage(): JSX.Element {
   return (
     <div className="bg-white text-center py-6 flex-grow container mx-auto md:py-10">
-      <h1 className="text-3xl font-bold py-2 mt-2 mb-3 md:text-5xl md:mb-10 lg:text-6xl">
-        PlayStation Developers
+      <h1 className="text-4xl font-bold py-2 mt-2 mb-8 lg:text-6xl md:mb-10">
+        PS Developers
       </h1>
-      <div className="bg-gradient-to-r from-blue-800 to-playstation p-4 rounded-3xl xl mx-auto mb-8 w-3/4 md:w-1/2 lg:w-1/3">
-        <ul className="text-white text-xl leading-6 md:leading-7 md:text-2xl lg:left-8">
-          {developers
-            .filter((developer: Developer) => developer.hasClosed === false)
-            .map((developer: Developer) => (
-              <Link key={developer.id} href={`developers/${developer.urlPath}`}>
-                <li className="font-semibold p-1 hover:text-amber-300 transition duration-200">
-                  {developer.name}
-                </li>
-              </Link>
-            ))}
-        </ul>
-      </div>
-      <div className="bg-gradient-to-r from-blue-800 to-playstation p-4 rounded-3xl xl mx-auto w-3/4 md:w-1/2 lg:w-1/3">
-        <ul className="text-white text-xl leading-6 md:leading-7 md:text-2xl lg:left-8">
-          {developers
-            .filter((developer: Developer) => developer.hasClosed === true)
-            .map((developer: Developer) => (
-              <Link key={developer.id} href={`developers/${developer.urlPath}`}>
-                <li className="font-semibold p-1 hover:text-amber-300 transition duration-200">
-                  {developer.name}
-                </li>
-              </Link>
-            ))}
-        </ul>
+      <div className="md:flex md:items-center md:justify-center md:space-x-8">
+        <div className="bg-gradient-to-r from-blue-800 to-playstation p-4 rounded-3xl xl mx-auto mb-8 w-3/4 md:w-2/5 lg:w-1/3 md:mx-0">
+          <ul className="text-white text-xl leading-6 md:leading-7 md:text-2xl lg:left-8">
+            {developers
+              .filter((developer: Developer) => developer.hasClosed === false)
+              .map((developer: Developer) => (
+                <Link
+                  key={developer.id}
+                  href={`developers/${developer.urlPath}`}
+                >
+                  <li className="font-semibold p-1 hover:text-amber-300 transition duration-200">
+                    {developer.name}
+                  </li>
+                </Link>
+              ))}
+          </ul>
+        </div>
+        <div className="bg-gradient-to-r from-blue-800 to-playstation p-4 rounded-3xl xl mx-auto w-3/4 md:w-2/5 lg:w-1/3 md:mx-0">
+          <ul className="text-white text-xl leading-6 md:leading-7 md:text-2xl lg:left-8">
+            {developers
+              .filter((developer: Developer) => developer.hasClosed === true)
+              .map((developer: Developer) => (
+                <Link
+                  key={developer.id}
+                  href={`developers/${developer.urlPath}`}
+                >
+                  <li className="font-semibold p-1 hover:text-amber-300 transition duration-200">
+                    {developer.name}
+                  </li>
+                </Link>
+              ))}
+          </ul>
+        </div>
       </div>
     </div>
   );

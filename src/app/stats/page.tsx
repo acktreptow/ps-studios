fdbae6af777ebe9a81f70cbce3ae1c8ce4bdbfd5
acktreptow <acktreptow@gmail.com>
2025-07-08 21:09:16 +0100

    update metacritic score breakdown

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -93,10 +93,10 @@ function StatsPage(): JSX.Element {
               Game with lowest Metacritic score (second-party): Destruction
               AllStars (Lucid Games) & Firewall Ultra (First Contact) - 62
             </li>
-            <li>Total 90+ PS5 games on Metacritic: 5</li>
-            <li>Total 80-89 PS5 games on Metacritic: 21</li>
-            <li>Total 70-79 PS5 games on Metacritic:6</li>
-            <li>Total 60-69 PS5 games on Metacritic: 3</li>
+            <li>Total 90+ PS5 games on Metacritic: 6</li>
+            <li>Total 80-89 PS5 games on Metacritic: 19</li>
+            <li>Total 70-79 PS5 games on Metacritic: 9</li>
+            <li>Total 60-69 PS5 games on Metacritic: 2</li>
             <li>Total 59- PS5 games on Metacritic: 0</li>
             <li>PS5 games released in 2020: 6</li>
             <li>PS5 games released in 2021: 8</li>

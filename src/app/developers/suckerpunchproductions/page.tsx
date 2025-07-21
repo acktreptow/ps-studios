5b1a9e13c707521d00cf7c8aebee4c0a4dfa31d2
acktreptow <acktreptow@gmail.com>
2025-07-21 16:33:04 +0100

    rewrite and condense sucker punch history and analysis sections

--- src/app/developers/suckerpunchproductions/page.tsx
+++ src/app/developers/suckerpunchproductions/page.tsx
@@ -30,7 +30,7 @@ function SuckerPunchProductionsPage(): JSX.Element {
       <SubTitle subtitle="History" />
       <Paragraph>
         Sucker Punch are a Washington-based developer that was founded in 1997,
-        initially to work on an N64 game called Rocket: Robot on Wheels. Sony
+        initially to work on an N64 platformer Rocket: Robot on Wheels. Sony
         were impressed, so they funded SP&#39;s Sly Cooper trilogy for the PS2
         and inFamous 1/2 on the PS3. After five great games under this
         partnership, Sony acquired the studio in 2011.
@@ -39,11 +39,10 @@ function SuckerPunchProductionsPage(): JSX.Element {
         SP have since moved on from inFamous to 2020&#39;s Ghost of Tsushima.
         This third-person samurai action-adventure is the PS4&#39;s
         fastest-selling first-party debut at 2.4 million copies sold in 3 days.
-        Tsushima then got a PS5 Director&#39;s Cut in 2021, which is currently
-        SP&#39;s only released PS5 game. It added around 10 more hours of
-        gameplay via a new island to explore and was even better critically
-        received. A PS5 native version also helped sell 13 million copies by
-        September 2024, making Tsushima extremely profitable.
+        Tsushima then got a PS5 Director&#39;s Cut in 2021, which included a
+        well recieved 10-hour story expansion via a new island to explore. This
+        DC also helped push Tsushima past 13 million copies, making it extremely
+        profitable.
       </Paragraph>
       <ImageSection flexOnMdScreens={true}>
         <div>
@@ -51,7 +50,7 @@ function SuckerPunchProductionsPage(): JSX.Element {
           <Paragraph>
             Tsushima is Sucker Punch&#39;s best selling game, so it&#39;s not
             surprising that they&#39;ve stuck with the IP for Ghost of Yōtei - a
-            standalone sequel taking place 329 years later. Despite its recent
+            standalone sequel taking place 329 years later. Despite its
             September 2024 reveal, Yōtei is already coming out in October 2025.
           </Paragraph>
           <Paragraph marginBottom={8}>
@@ -59,8 +58,8 @@ function SuckerPunchProductionsPage(): JSX.Element {
             surpasses Tsushima critically because they can now refine the
             already successful formula. The larger PS5 install base means Yōtei
             will surpass the Tsushima DC commercially too. Finally, due to the
-            length of game development with how far we&#39;re into the PS5
-            lifecycle, I also imagine Yōtei is SP&#39;s final PS5 game.
+            length of game development and how far we&#39;re into PS5&#39;s
+            lifecycle, I also predict Yōtei is SP&#39;s final PS5 game.
           </Paragraph>
         </div>
         <div>

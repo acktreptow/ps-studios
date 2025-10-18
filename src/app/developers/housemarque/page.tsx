0cfc8b1d76e714df4aa686dced05d0a35db47198
acktreptow <acktreptow@gmail.com>
2025-10-18 16:45:23 +0100

    update housemarque page with saros gameplay analysis

--- src/app/developers/housemarque/page.tsx
+++ src/app/developers/housemarque/page.tsx
@@ -38,14 +38,15 @@ function HousemarquePage(): JSX.Element {
         So far, Housemarque has only released 2021&#39;s Returnal on PS5. It
         kept the roguelike/arcade DNA seen in their previous games, but moved to
         third-person with more focus on story. This gave Returnal more
-        mainstream appeal, resulting in it becoming their best selling game at
-        560k copies sold in 3 months and Sony acquiring them very shortly after.
+        mainstream appeal, resulting in it becoming Housemarque&#39;s best
+        selling game at 560k copies sold in 3 months and Sony acquiring them
+        shortly after.
       </Paragraph>
       <Paragraph marginBottom={8}>
         Housemarque continued to support Returnal with a PC version and co-op
-        mode before announcing their next game, Saros, in February 2025 via a CG
-        trailer. Saros will play very similar to Returnal, except this time,
-        it&#39;ll be a roguelite instead of a roguelike.
+        mode before announcing their next game, Saros, via a CG trailer in
+        February 2025 and a gameplay reveal in September. Saros plays very
+        similar to Returnal, except it&#39;s a roguelite instead of a roguelike.
       </Paragraph>
       <ImageSection flexOnMdScreens={true}>
         <div>
@@ -54,15 +55,16 @@ function HousemarquePage(): JSX.Element {
             With Returnal, Housemarque proved they could repackage the arcade
             DNA of their older/smaller games into a AAA mainstream title. Saros
             being a roguelite also furthers its mainstream appeal on a much
-            larger 2026 PS5 install base, resulting in even higher sales.
+            larger 2026 PS5 install base, which should result in even higher
+            sales.
           </Paragraph>
           <Paragraph marginBottom={8}>
             Housemarque are easily one of the bright spots for PS Studios this
-            generation, especially as they were acquired during the frenzy when
-            Sony were laser focused on live-service. Thankfully, they
-            weren&#39;t get caught up in that. Saros also ships in 2026 meaning
-            we should see more very soon. Housemarque are therefore one of the
-            few PS Studios most worth watching currently.
+            generation, especially as they were acquired during Sony&#39;s
+            laser-focused live-service push. Thankfully, they weren&#39;t get
+            caught up in that - making them one of the few PS Studios
+            currently worth watching. Saros also ships in February 2026, so
+            there&#39;s not long to wait.
           </Paragraph>
         </div>
         <div>

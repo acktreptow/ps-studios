6656318ab60d74bf5bbf5a22be574307db003cf1
acktreptow <acktreptow@gmail.com>
2025-10-22 21:50:03 +0100

    edit down pixelopus page

--- src/app/developers/pixelopus/page.tsx
+++ src/app/developers/pixelopus/page.tsx
@@ -26,15 +26,15 @@ function PixelopusPage(): JSX.Element {
       <Paragraph>
         Pixelopus was a California-based developer internally formed in 2014
         under unique circumstances. In 2012, Thatgamecompany&#39;s released PS3
-        indie adventure Journey to monumental critical and commercial acclaim.
-        This inspired Sony first-party developer Foster City Studio (now San
-        Mateo Studio) to fund small development teams and emulate this success.
+        indie adventure Journey to critical and commercial acclaim. This
+        inspired Sony&#39;s Foster City Studio (now San Mateo Studio) to fund
+        small development teams and emulate this success.
       </Paragraph>
       <Paragraph>
         One university they funded was Carnegie Mellon, who they gave PS Vitas
         to experiment with. 6 students were hired, along with 3 from San Jose
         University and 2 industry vets to form Pixelopus. That same year, they
-        released Entwined for PS3, PS4 and Vita. The twin-stick rhythm game
+        released Entwined for PS3, PS4 and Vita. The twin-stick rhythm title
         shadow-dropped at Sony&#39;s 2014 E3 showcase but only received tepid
         critical and commercial success.
       </Paragraph>
@@ -42,37 +42,34 @@ function PixelopusPage(): JSX.Element {
         Pixelopus followed Entwined with 2019&#39;s Concrete Genie on PS4. This
         award-winning action-adventure was heavily inspired by Jet Set Radio and
         street art, but like Entwined, it wasn&#39;t a commercial hit. It was
-        unfortunately also Pixelopus&#39;s final game, as Sony shuttered them in
-        2023.
+        also Pixelopus&#39;s final game, as Sony shuttered them in 2023.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            It&#39;s not surprising Sony closed Pixelopus as their AA
-            single-player DNA clashed with the live-service push. However in
-            2025, it&#39;s clear with games like Concord, Fairgames and Marathon
-            that this initiative failed - resulting in consequential gaps
-            between first-party games. Like Japan Studio who shut in 2021,
-            Pixelopus would&#39;ve been more useful to Sony now than in 2023 as
-            their AA games could help fill these gaps.
+            It&#39;s unsurprising Sony shut Pixelopus as their single-player DNA
+            clashed with the 2020-23 live-service push. But in 2025, it&#39;s
+            clear with games like Concord that the initiative failed - resulting
+            in consequential gaps between first-party titles. Like Japan Studio
+            who shut in 2021, Pixelopus would be more useful now than in 2023 as
+            their smaller games would help fill these gaps.
           </Paragraph>
           <Paragraph>
-            Nonetheless, the unfortunate reality is that closing Pixelopus can
-            be justified. Unlike Japan Studio, their legacy amongst PlayStation
-            fans is non-existent. JS games might not sell millions, but titles
-            like Shadow of the Colossus provide utility that goes beyond money
-            as it keeps the hardcore base engaged. Whereas Pixelopus games not
-            only don&#39;t sell, but aren&#39;t evangelized by hardcore PS5 fans
-            as a brand differentiator, regardless of awards.
+            However, closing Pixelopus can still be justified. Unlike Japan
+            Studio, their legacy amongst PlayStation fans is non-existent. JS
+            games don&#39;t sell millions, but do provide utility that goes
+            beyond money as they keep the hardcore base engaged. Whereas
+            Pixelopus games not only don&#39;t sell, but aren&#39;t evangelized
+            as a brand differentiator despite the awards.
           </Paragraph>
           <Paragraph marginBottom={8}>
             Pixelopus&#39;s expensive San Mateo location further makes it hard
-            to justify keeping open in a worsening economy. Finally, their
-            cancelled PS5 game was a licensed IP in partnership with Sony
-            Animation, which would incur extra fees/red tape. Considering these
-            factors, even with Sony&#39;s failed live-service push, closing
-            Pixelopus is still a logical conclusion.
+            to justify keeping open in this economy, and their cancelled PS5
+            game was a licensed IP in partnership with Sony Animation, which
+            would incur extra fees/red tape. Considering these factors, even
+            with Sony&#39;s failed live-service push, closing Pixelopus is still
+            a logical conclusion.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

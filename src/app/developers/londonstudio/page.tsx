2e988a96deef524e85a0734a832bb1c1439db837
acktreptow <acktreptow@gmail.com>
2026-01-31 22:18:20 +0000

    update london studio page for 2026

--- src/app/developers/londonstudio/page.tsx
+++ src/app/developers/londonstudio/page.tsx
@@ -26,60 +26,59 @@ function LondonStudioPage(): JSX.Element {
       <Paragraph>
         London Studio was founded in 2002, when SCE Studio Camden (formerly
         Psygnosis) and Team Soho merged. Their portfolio includes traditional IP
-        such as PS2&#39;s open-world &#39;The Getaway&#39; series and supporting
-        EU first-party games like LittleBigPlanet and Killzone 2. However, most
-        of LS&#39;s games were developed to take advantage of the various
-        peripherals each PlayStation had.
+        such as PS2&#39;s GTA-like The Getaway series. However, most LS games
+        took advantage of the various peripherals each PlayStation had.
       </Paragraph>
       <Paragraph>
-        London Studios&#39;s most successful IP was Singstar, selling more than
-        20 million games across PS2-PS4. It used a USB mic, but LS also
-        developed games that required more unique peripherals. For PS2, it was
-        various camera-based EyeToy games. For PS3, it was AR Wonderbook games.
-        And for PS4, it was PSVR&#39;s Blood and Truth. All were commercially
-        and/or critically successful. PS3&#39;s PlayStation Home must also be
-        mentioned, as it was one of the first metaverses - launching 10+ years
-        before that was even a term.
+        LS&#39;s most successful IP was Singstar, which sold 20 million+ across
+        PS2-PS4. It used a USB mic, but LS also utilized more unique
+        peripherals. For PS2, it was the camera-based EyeToy. For PS3, it was
+        the AR Wonderbook. For PS4, it was PSVR&#39;s Blood and Truth. All were
+        commercially and/or critically successful. PS3&#39;s PlayStation Home
+        was also one of the first metaverses - launching 10+ years before it was
+        even a term.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        London Studios&#39;s final game was 2019&#39;s Erica, which utilized
-        PS4&#39;s PlayLink to add smartphone compatibility, and was only tepidly
-        received. Combined with PSVR&#39;s plummeting popularity, the upcoming
-        PS5, and Sony&#39;s heavy live-service push, it&#39;s no surprise LS
-        pivoted to live-service for PS5. While it wouldn&#39;t use a peripheral,
-        it was also still consistent with LS&#39;s DNA as live-service was new
-        to them. Unfortunately, the game would not only be cancelled, but also
-        cause LS&#39;s closure in 2024.
+        Their final game was 2019&#39;s Erica, which used PS4&#39;s PlayLink to
+        add smartphone compatibility, but was only tepidly received. Considering
+        the upcoming PS5 and Sony&#39;s live-service push, it&#39;s no surprise
+        LS pivoted there too - especially as it was still consistent with their
+        DNA, as live-service was new to them. Unfortunately, the title would not
+        only be cancelled, but also caused LS&#39;s closure in 2024.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            Despite London Studios&#39;s success across PS2-PS4, in hindsight,
-            it&#39;s easy to see that the pivot to live-service would fail.
             Project Camden was an online co-op combat game set in a modern
-            fantasy London and used the Soho engine LS built for PS5. It was
-            also their most ambitious project to date and would&#39;ve had the
-            longest development time in studio history.
+            fantasy London, used the Soho engine LS built for PS5, and had the
+            longest development time in LS history. Sony&#39;s 2020-23
+            live-service push also meant Camden was easily greenlit, yet
+            there&#39;s major risk following trends with how long game
+            development now is.
           </Paragraph>
           <Paragraph>
-            Sony&#39;s 2020-23 live-service push meant Camden was easily
-            greenlit. Yet there&#39;s major risk following trends with how long
-            game development is. By 2024, there was a worse economy, less
-            playtime post-lockdown, and a hatred for Sony&#39;s live-service
-            push. LS was also in an expensive city, in a genre they had no
-            experience in. So when Sony had to cut costs in a live-service space
-            it was increasingly bearish in, LS was logically chosen for closure
-            over others.
+            By 2024, there was a worse economy, less playtime post-lockdown, and
+            a hatred for live-services among PS5&#39;s hardcore base. LS was
+            also in an expensive city, in a genre they had no experience in. So
+            when Sony cut costs, LS was logically chosen for closure over
+            others.
+          </Paragraph>
+          <Paragraph>
+            LS&#39;s closure is a particular shame as they had a path for great
+            success. In 2005, LS revealed 2 traditional AAA PS3 titles - The
+            Getaway and Eight Days - with excellently received trailers. But
+            Sony cancelled both to reallocate money to others. Had these
+            blockbusters reached their potential and launched to a starving
+            06-09 PS3 hardcore audience, history would be very different.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            London Studios&#39;s closure is a particular shame as they had a
-            path for great success. In 2005, LS revealed 2 traditional AAA PS3
-            games - The Getaway and Eight Days - with excellently received
-            trailers. Unfortunately, Sony cancelled both to reallocate money
-            to others. Had these blockbusters reached their potential and
-            launched to a starving 06-09 PS3 audience alongside Singstar,
-            LS&#39;s history would be very different.
+            London Studio now lives on through Twisted Works - a new developer
+            composed of many former LS staff. In Dec 2025, they revealed Cast
+            Outs, which is basically what Camden was. An early access beta is
+            scheduled for PS5/PC in early 2027, so it&#39;ll be about a year
+            before it can be determined if Sony made the right choice in closing
+            LS.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

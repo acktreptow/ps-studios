586e4edc4dc293701916b5c571f3faf65802e0c7
acktreptow <acktreptow@gmail.com>
2026-02-02 20:13:26 +0000

    update firewalk page for 2026

--- src/app/developers/firewalkstudios/page.tsx
+++ src/app/developers/firewalkstudios/page.tsx
@@ -31,19 +31,16 @@ function FirewalkStudiosPage(): JSX.Element {
       <Paragraph>
         This Washington-based developer was founded in 2018, by senior veterans
         of shooters like Halo and Destiny. With this prestige multiplayer
-        expertise and Sony&#39;s increased focus on live-service, it&#39;s no
-        surprise they acquired Firewalk in April 2023.
-      </Paragraph>
-      <Paragraph>
-        Firewalk then announced their first game, Concord, shortly after in May
-        2023. A 5vs5 live-service FPS that released on PS5/PC in August 2024.
-        Yet it wasn&#39;t until May 2024 before we actually saw Concord, via
-        story and gameplay trailers that were universally hated.
+        expertise and Sony&#39;s increased focus on live-service, it&#39;s not
+        surprising they acquired Firewalk in April 2023.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        Playable betas in July didn&#39;t help either. Character designs were
-        particularly slated and the gameplay wasn&#39;t good enough to justify
-        $40. Especially compared to its F2P competitors like Fortnite and
+        Firewalk announced Concord shortly after in May 2023 - a 5vs5
+        live-service FPS that released on PS5/PC in August 2024. Yet it took
+        until May 2024 to actually see Concord via story and gameplay trailers
+        that were universally hated, with character designs especially panned.
+        Playtests also showcased how gameplay wasn&#39;t good enough to justify
+        $40 - especially compared to its F2P competitors like Fortnite and
         Overwatch.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
@@ -54,13 +51,14 @@ function FirewalkStudiosPage(): JSX.Element {
             Firewall Ultra, Destruction AllStars and Lost Soul Aside for the
             lowest first-party PS5 Metacritic score at 62. Concord also only
             sold 25,000 - a figure so low that Sony removed it from sale and
-            refunded purchases 11 days later.
+            forced refunds 11 days later because they&#39;d recoup more with
+            Concord as a tax writeoff.
           </Paragraph>
           <Paragraph>
-            Firewalk explored how to bring back Concord in a way that resonates,
-            but on October 29th, Firewalk closed along with confirmation that
-            Concord wouldn&#39;t return. It&#39;s therefore easy to predict that
-            they&#39;ll be no more more Firewalk games on PS5.
+            Firewalk explored how to successfully bring back Concord, but in
+            October, Firewalk closed along with confirmation that Concord
+            wouldn&#39;t return. It&#39;s therefore easy to predict that
+            there&#39;ll be no more PS5 titles from them.
           </Paragraph>
           <Paragraph marginBottom={8}>
             This was the first time Sony refunded a game solely due to its

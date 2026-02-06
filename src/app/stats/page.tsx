c70a740f7715d50d4dcced900dc4cdd83ef98ed2
acktreptow <acktreptow@gmail.com>
2026-02-06 08:56:37 +0000

    update rest of studio stats

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -159,8 +159,8 @@ function StatsPage(): JSX.Element {
             <li>Studios in NA: 14 (excluding XDev US)</li>
             <li>Studios in EU: 6 (excluding XDev Liverpool)</li>
             <li>Studios in Asia: 3 (excluding XDev Tokyo)</li>
-            <li>PS5 games from NA studios: 25</li>
-            <li>PS5 games from EU studios: 11</li>
+            <li>PS5 games from NA studios: 27</li>
+            <li>PS5 games from EU studios: 12</li>
             <li>PS5 games from Asia studios: 11</li>
             <li>Released PS5 games from NA studios: 19</li>
             <li>Released PS5 games from EU studios: 10</li>

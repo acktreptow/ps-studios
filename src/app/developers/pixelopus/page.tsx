651147b42d88ad4bc74cbe1f6194bddf88bb97cc
acktreptow <acktreptow@gmail.com>
2026-07-20 06:02:39 +0100

    update pixelopus history

--- src/app/developers/pixelopus/page.tsx
+++ src/app/developers/pixelopus/page.tsx
@@ -25,24 +25,24 @@ function PixelopusPage(): JSX.Element {
       <SubTitle subtitle="History" />
       <Paragraph>
         Pixelopus was a California-based developer internally formed in 2014
-        under unique circumstances. In 2012, Thatgamecompany released PS3
-        indie adventure Journey to critical and commercial acclaim. This
-        inspired Sony&#39;s Foster City Studio (now San Mateo Studio) to fund
-        small development teams and emulate this success.
+        under unique circumstances. In 2012, Thatgamecompany released PS3 indie
+        adventure Journey to critical and commercial acclaim. This inspired
+        Sony&#39;s Foster City Studio (now San Mateo Studio) to fund small
+        development teams and emulate this success.
       </Paragraph>
       <Paragraph>
-        One university they funded was Carnegie Mellon, who they gave PS Vitas
-        to experiment with. 6 students were hired, along with 3 from San Jose
-        University and 2 industry vets to form Pixelopus. That same year, they
-        released Entwined for PS3, PS4 and Vita. The twin-stick rhythm title
-        shadow-dropped at Sony&#39;s 2014 E3 showcase but only received tepid
-        critical and commercial success.
+        One university they funded was Carnegie Mellon, which they gave PS Vitas
+        to experiment with in 2013. 6 students were hired, along with 3 from San
+        Jose State University and 2 industry vets, to form Pixelopus in 2014.
+        That same year, they released Entwined for PS3, PS4, and Vita. The
+        twin-stick rhythm title shadow-dropped at Sony&#39;s 2014 E3 showcase,
+        but only received tepid critical and commercial success.
       </Paragraph>
       <Paragraph marginBottom={8}>
         Pixelopus followed Entwined with 2019&#39;s Concrete Genie on PS4. This
-        award-winning action-adventure was heavily inspired by Jet Set Radio and
+        award-winning action-adventure was inspired by Jet Set Radio and
         street art, but like Entwined, it wasn&#39;t a commercial hit. It was
-        also Pixelopus&#39;s final game, as Sony shuttered them in 2023.
+        also Pixelopus&#39;s final game, as Sony closed them in 2023.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>

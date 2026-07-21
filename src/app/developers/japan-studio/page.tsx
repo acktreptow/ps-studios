afb126353da68361b7bbb1ca53afa3241a1787e8
acktreptow <acktreptow@gmail.com>
2026-07-21 02:16:50 +0100

    update japan studio analysis

--- src/app/developers/japan-studio/page.tsx
+++ src/app/developers/japan-studio/page.tsx
@@ -59,28 +59,28 @@ function JapanStudioPage(): JSX.Element {
             JS would be a tremendous attribute for PS5 right now.
           </Paragraph>
           <Paragraph>
-            Outside of Helldivers 2, Sony&#39;s live-strategy has failed. Most
-            second-party games are cancelled, newly acquired studios have
-            delayed games, and many old studios like Bend have wasted this
-            generation on GaaS. This has caused Sony&#39;s first-party output to
-            become sporadic since 2022, resulting in the prestige of PS Studios
-            taking a significant hit.
+            Outside of Helldivers 2, Sony&#39;s live-service strategy has
+            failed. Most second-party games are cancelled, newly acquired
+            studios have delayed games or closed, and old studios like Bend have
+            wasted the generation on it. This has caused Sony&#39;s first-party
+            output to become sporadic since 2022, resulting in the prestige of
+            PS Studios taking a significant hit.
           </Paragraph>
           <Paragraph>
             JS&#39;s AA games don&#39;t sell millions with a 90+ Metacritic, but
-            the smaller scope means many would ship, thus allowing Sony to still
-            have consistent releases. With so few AAA titles currently,
-            there&#39;s also an opening for the hardcore base to evangelize the
-            AA scene more than they would&#39;ve pre-2023. Astro Bot&#39;s
-            success demonstrates this perfectly.
+            the smaller scope means many would ship on modest budgets, allowing
+            Sony to have consistent releases without overspending. With so few
+            AAA titles, hardcore fans would also evangelize AA titles more than
+            they would have pre-2023. Astro Bot&#39;s success demonstrates this
+            perfectly.
           </Paragraph>
           <Paragraph marginBottom={8}>
             Fortunately, many ex-JS staff formed new studios to continue making
             games together, like Team Gravity&#39;s Slitterhead as Bokeh Game
             Studio, or Patapon&#39;s spiritual sequel, Ratatan. Bandai Namco
-            also licensed Everybody&#39;s Golf, Freedom Wars & Patapon from Sony
-            to release sequels/remakes. Between these titles and Asobi, Japan
-            Studio&#39;s legacy lives on.
+            also licensed Everybody&#39;s Golf, Freedom Wars, and Patapon from
+            Sony to release sequels and remakes. Between these titles and Asobi,
+            Japan Studio&#39;s legacy lives on.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

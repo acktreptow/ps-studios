03427ac0b0f6e344efd3b1b19e30c564ccd84ecf
acktreptow <acktreptow@gmail.com>
2026-07-22 21:12:04 +0100

    update firesprite history

--- src/app/developers/firesprite/page.tsx
+++ src/app/developers/firesprite/page.tsx
@@ -29,36 +29,29 @@ function FirespritePage(): JSX.Element {
       />
       <SubTitle subtitle="History" />
       <Paragraph>
-        Founded in 2012, Firesprite is a Liverpool-based developer formed by
-        ex-Studio Liverpool (previously Psygnosis) employees, which Sony closed
-        only weeks prior to reduce headcount in the EU. Firesprite&#39;s first
-        major game was 2018&#39;s The Persistence, which launched on PS4 as well
-        as Xbox One and Nintendo Switch since Sony wouldn&#39;t acquire
-        Firesprite until 2021.
+        Founded in 2012, Firesprite are a Liverpool-based developer formed by
+        ex-Studio Liverpool (previously Psygnosis) employees after Sony closed
+        the studio only weeks prior to reduce headcount in the EU.
+        Firesprite&#39;s first major game was 2018&#39;s The Persistence, which
+        launched on PS4 as well as Xbox One and Nintendo Switch since Sony
+        wouldn&#39;t acquire Firesprite until 2021.
+      </Paragraph>
+      <Paragraph>
+        So far, Firesprite have released 2 PS5 games - an enhanced port of The
+        Persistence in 2021, and flagship PSVR2 launch title Horizon Call of the
+        Mountain in 2023. Neither was particularly well received, critically or
+        commercially. In fact, Firesprite have the lowest average Metacritic
+        score out of the PS Studios developers who have shipped a PS5 title.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        So far, Firesprite has released two PS5 games - an enhanced port of The
-        Persistence in 2021, and flagship PSVR2 launch title Horizon Call of
-        the Mountain in 2023. Neither was particularly well received,
-        critically or commercially. In fact, Firesprite has the lowest average
-        Metacritic score out of the PS Studios developers who&#39;ve
-        shipped a PS5 title.
+        Crunch was also necessary to ship Horizon VR despite its reception. All
+        but one founder and many other staff also left once retention bonuses
+        were paid out, and layoffs then took place a year later, after reports
+        that their unannounced live-service Twisted Metal game was cancelled.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
-          <Paragraph>
-            Firesprite hasn&#39;t announced anything since Horizon launched
-            alongside PSVR2. Most likely due to their dire situation,
-            post-acquisition.
-          </Paragraph>
-          <Paragraph>
-            Crunch was necessary to ship Horizon VR. Additionally, all but one
-            founder and many other staff left once retention bonuses were paid
-            out. Layoffs then took place a year later, after reports that
-            Firesprite&#39;s unannounced live-service Twisted Metal game was
-            cancelled.
-          </Paragraph>
           <Paragraph marginBottom={8}>
             Since then, Firesprite has been linked to a sequel to PS4&#39;s
             Until Dawn, which was remade on PS5 in 2024 and included a new

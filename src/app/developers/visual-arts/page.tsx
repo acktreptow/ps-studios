c4c705df945f7ccc3fd58bac7d19bbd5efef3ebb
acktreptow <acktreptow@gmail.com>
2026-07-23 05:57:14 +0100

    update vasg history

--- src/app/developers/visual-arts/page.tsx
+++ src/app/developers/visual-arts/page.tsx
@@ -24,40 +24,40 @@ function VisualArtsPage(): JSX.Element {
       />
       <SubTitle subtitle="History" />
       <Paragraph>
-        Founded in 2007, this San Diego-based developer supports games from
-        other PS Studios with art, animation etc. As support, their
-        contributions are small, but it means VASG works on 15-25 games at a
-        time, in addition to Sony&#39;s non-gaming IP like Netflix&#39;s Love,
-        Death & Robots. Though not the traditional role of a developer, there
-        was a failed pivot to become one in 2018.
+        Founded in San Diego in 2007, the Visual Arts Service Group support
+        games from other PS Studios with art and animation. As a support studio,
+        their contributions are small, but it means VASG work on 15-25 games at
+        a time, in addition to Sony&#39;s non-gaming IP like Netflix&#39;s Love,
+        Death & Robots. While it&#39;s not the traditional role of a developer,
+        there was a failed pivot to become one in 2018.
       </Paragraph>
       <Paragraph>
         VASG Senior Director Michael Mumbauer aspired to become a proper PS
         Studio with full creative control over a game, so 30 devs joined from
         nearby studios to remake 2007&#39;s Uncharted. However, as an early PS3
-        game, it was too hard to modernize on PS5 at that size. VASG then
+        game, it was too hard to modernize for PS5 at that size. VASG then
         settled on 2013&#39;s The Last of Us - a more modern PS3 game.
         Afterwards, VASG could hire up for Uncharted, then their own titles.
       </Paragraph>
       <Paragraph>
-        TLOU remake (Project T1X) was probationally approved, meaning it stayed
-        a secret with no budget to hire up. VASG questioned Sony&#39;s
+        The TLOU remake (Project T1X) was probationally approved, meaning it
+        stayed a secret with no budget to hire up. VASG questioned Sony&#39;s
         commitment, but completed a vertical slice to showcase T1X&#39;s look
         and feel in spring 2019 - just before Hermen Hulst took over PS Studios.
-        Due to his new role, Hulst thoroughly reviewed the entire PS Studios
+        As part of his new role, Hulst thoroughly reviewed the entire PS Studios
         portfolio and grew concerned with T1X&#39;s budget. Mumbauer argued this
-        was VASG&#39;s first project, on a new engine, and had to hire up to
-        modernize gameplay. Hulst was unconvinced.
+        was VASG&#39;s first project, on a new engine, and they had to hire up
+        to modernize gameplay. Hulst was unconvinced.
       </Paragraph>
       <Paragraph marginBottom={8}>
-        Instead, VASG supported Naughty Dog&#39;s TLOU 2 after its delay to
-        2020. Once shipped, instead of hiring up, T1X shifted to ND&#39;s budget
-        who joined in a support role. Mumbauer knew this would strip autonomy as
-        ND outpaced VASG&#39;s work due to their size. As TLOU was their IP, ND
-        were also trusted more by Sony, effectively giving them final say. VASG
-        was now jokingly referred to as ND South, so in late 2020, with his
-        vision dead, Mumbauer and many others quit. VASG now continues in the
-        support role Sony always saw them in.
+        Instead, VASG supported Naughty Dog&#39;s TLOU2 after its delay to 2020.
+        Once shipped, instead of hiring up, T1X shifted to ND&#39;s budget and
+        ND joined in a support role. Mumbauer knew this would strip autonomy as
+        ND outpaced VASG&#39;s work due to their size. ND also had Sony&#39;s
+        trust as the IP&#39;s original developer, effectively giving them final
+        say. VASG were now jokingly referred to as ND South, so in late 2020,
+        with his vision dead, Mumbauer and many others quit. VASG now continue
+        in the support role Sony always saw them in.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
         <div>

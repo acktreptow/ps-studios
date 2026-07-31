19a33624a2060a211ea3cbcf225b9a9da24dbcff
acktreptow <acktreptow@gmail.com>
2026-07-31 04:51:03 +0100

    update bend analysis

--- src/app/developers/bend-studio/page.tsx
+++ src/app/developers/bend-studio/page.tsx
@@ -44,14 +44,14 @@ function BendStudioPage(): JSX.Element {
         <div>
           <SubTitle subtitle="My Analysis & Predictions" />
           <Paragraph>
-            Despite Days Gone shipping over 6 years ago, we don&#39;t even have
-            a name for Bend&#39;s next game due to a very long string of bad
-            luck and incompetence:
+            Despite DG shipping over 6 years ago, we don&#39;t even have a name
+            for Bend&#39;s next game due to a very long string of bad luck and
+            incompetence:
           </Paragraph>
           <List>
             <li>
-              Bend pitched Days Gone 2, which Sony rejected due to Days
-              Gone&#39;s initial reception.
+              Bend pitched Days Gone 2, which Sony rejected due to DG&#39;s
+              initial reception.
             </li>
             <li>
               A 2020 internal power struggle resulted in DG&#39;s writer and
@@ -70,16 +70,16 @@ function BendStudioPage(): JSX.Element {
             Fortunately, much of DG&#39;s criticism comes from those who lost
             the power struggle. Between that, Sony&#39;s increased reluctance in
             live services, DG getting a remaster, and the more positive general
-            consensus around the IP in 2026, there&#39;s a better chance Days
-            Gone 2 is greenlit than in recent years, which would be the
-            best-case scenario.
+            consensus around the IP in 2026, there&#39;s a better chance DG2 is
+            greenlit than in recent years, which is the best-case scenario.
           </Paragraph>
           <Paragraph marginBottom={8}>
-            The worst-case scenario would be Bend&#39;s closure, which is
-            unfortunately more likely. At the very least, don&#39;t expect a new
-            Bend title until PS6, making the PS5 a truly wasted generation for
-            Bend due to ex-leadership and Sony&#39;s poor live service
-            execution.
+            The worst-case scenario is Bend&#39;s closure, which is more likely
+            given they&#39;ve shipped 1 game in 15 years that came with a lot of
+            controversy. They&#39;re also years away from shipping a second. At
+            the very least, don&#39;t expect a new Bend title until PS6, making
+            the PS5 a truly wasted generation for them due to former leadership
+            and Sony&#39;s poor live service execution.
           </Paragraph>
         </div>
         <div className="md:flex md:flex-col md:items-center">

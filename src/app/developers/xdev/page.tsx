69ec3f1061ea1961dc3f40d6798387ad9656da9f
acktreptow <acktreptow@gmail.com>
2026-08-15 17:50:29 +0100

    update xdev history

--- src/app/developers/xdev/page.tsx
+++ src/app/developers/xdev/page.tsx
@@ -49,7 +49,7 @@ function XDevpage(): JSX.Element {
       <Paragraph marginBottom={8}>
         2024 was XDev&#39;s highest yearly output at 4, but there&#39;s only 1
         for 2026 - Marvel Tōkon: Fighting Souls (Arc System Works), which scored
-        a great 87 on Metacritic. Tōkon also shipped on PC, something XDev will
+        a great 86 on Metacritic. Tōkon also shipped on PC, something XDev will
         be increasingly known for given what Sony recently announced.
       </Paragraph>
       <ImageSection flexOnMdScreens={false}>
